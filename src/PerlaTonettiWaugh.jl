c8db6083367fb2fab84cf7214c8c1d9ee615245c
Jesse Perla <jesseperla@gmail.com>
2018-06-04 14:47:09 -0700

    Reorganized to remvoe manually reversed code.

--- src/PerlaTonettiWaugh.jl
+++ src/PerlaTonettiWaugh.jl
@@ -1,8 +1,8 @@
 module PerlaTonettiWaugh
 
 # package code goes here
-export solvemodel
-using DifferentialEquations, NamedTuples, BenchmarkTools, Plots, BandedMatrices
+export diffusionoperators, createODEproblem, createDAEproblem
+using DifferentialEquations, NamedTuples, BenchmarkTools, BandedMatrices, Sundials
 
 #These would be generated automatically by DiffEqOperators.jl
 #These are the differential operators for positive drift 0upwind finite differences
@@ -28,7 +28,7 @@ function diffusionoperators(x_min, x_max, M)
 end
 
 #Create DiffEq Problem for solving
-function createODEproblem(c_tilde, sigma_tilde, mu_tilde, x_min::Float64, x_max::Float64, M::Int64, T::Float64, rho::Float64, reversed = false)
+function createODEproblem(c_tilde, sigma_tilde, mu_tilde, x_min::Float64, x_max::Float64, M::Int64, T::Float64, rho::Float64)
     x, L_1_plus, L_2  = diffusionoperators(x_min, x_max, M) #Discretize the operator
 
     p = @NT(L_1_plus = L_1_plus, L_2 = L_2, x = x, rho = rho, mu_tilde = mu_tilde, sigma_tilde = sigma_tilde, c_tilde = c_tilde, T = T) #Named tuple for parameters.
@@ -49,107 +49,51 @@ function createODEproblem(c_tilde, sigma_tilde, mu_tilde, x_min::Float64, x_max:
         du .-= p.c_tilde.(t, p.x)
     end
 
-    function f_reversed(du,u,p,t_tilde)
-        L = -(p.rho*I  - Diagonal(p.mu_tilde.(p.T - t_tilde, x)) * p.L_1_plus - Diagonal(p.sigma_tilde.(p.T - t_tilde, p.x).^2/2.0) * p.L_2)
-        A_mul_B!(du,L,u)
-        du .+= p.c_tilde.(p.T - t_tilde, p.x)
-    end
-
     #Checks on the residual
     du_T = zeros(u_T)
     f(du_T, u_T, p, T)
     #@show norm(du_T)
     @assert norm(du_T) < 1.0E-10
 
-    f_reversed(du_T, u_T, p, 0)
-    @assert norm(du_T) < 1.0E-10
-
-    if reversed==false
-        return ODEProblem(f, u_T, (T, 0.0), p)
-    else
-        return ODEProblem(f_reversed, u_T, (0.0,T), p) #Backwards in time, starting at T = 0
-    end
+    return ODEProblem(f, u_T, (T, 0.0), p)
 end
 
-function solvemodel()
-    # mu_tilde is time varying but sigma_tilde is not.
-    x_min = 0.01
-    x_max = 1.0
-    M = 20
-    T = 10.0
-    rho = 0.15
-    sigma_bar = 0.1
-    c_tilde(t, x) = x + 0.0001*t
-    sigma_tilde(t, x) =  sigma_bar
-    mu_tilde(t, x) = 0.1*x *(1.0 + 4.0 * t)
-    basealgorithm = CVODE_BDF() #CVODE_CDF(linear_solver=:GMRES) #ImplicitEuler() #A reasonable alternative. Algorithms which don't work well: Rosenbrock23(), Rodas4(), KenCarp4()
-    plotevery = 5
+function createDAEproblem(c_tilde, sigma_tilde, mu_tilde, x_min, x_max, M, T, rho)
+    x, L_1_plus, L_2  = diffusionoperators(x_min, x_max, M) #Discretize the operator
 
-    prob = createODEproblem(c_tilde, sigma_tilde, mu_tilde, x_min, x_max, M, T, rho, false)
-    sol = solve(prob, basealgorithm)
-    plot(sol, vars=1:plotevery:M)
-    @assert(issorted(sol[end]))
-    @benchmark solve($prob, $basealgorithm) #Benchmark
+    p = @NT(L_1_plus = L_1_plus, L_2 = L_2, x = x, rho = rho, mu_tilde = mu_tilde, sigma_tilde = sigma_tilde, c_tilde = c_tilde, M = M) #Named tuple for parameters.
 
-    prob_reversed = createODEproblem(c_tilde, sigma_tilde, mu_tilde, x_min, x_max, M, T, rho, true)
-    sol_reversed = solve(prob_reversed, basealgorithm)
-    plot(sol_reversed, vars=1:plotevery:M, xlabel="t_tilde")
+    #Check upwind direction
+    @assert minimum(mu_tilde.(T, x)) >= 0.0
+    @assert minimum(mu_tilde.(0.0, x)) >= 0.0
 
-    #Compare the timesteps and the terminal/starting values
-    @show sol.t'
-    @show T - sol_reversed.t' #i.e. t = T - t_tilde
-    @show norm(sol(0) - sol_reversed(T)) #i.e. t=0
-    @show norm(sol(T) - sol_reversed(0)) #i.e. t=T
+    #Calculating the stationary solution,
+    L_T = rho*I - Diagonal(mu_tilde.(T, x)) * L_1_plus - Diagonal(sigma_tilde.(T, x).^2/2.0) * L_2
+    u_T = L_T \ c_tilde.(T, x)
 
-    #Solve backwards with a DAE and a trivial algebraic equation
-    #u_ex(1:M) = u(), following the ODE
-    #u_ex(M+1) = 1.0 #(i.e., a trivial linear function)
-    function createDAEproblem(c_tilde, sigma_tilde, mu_tilde, x_min, x_max, M, T, rho)
-        x, L_1_plus, L_2  = diffusionoperators(x_min, x_max, M) #Discretize the operator
+    u_ex_T = [u_T; 1.0] #Closed form for the trivial linear function we are adding
 
-        p = @NT(L_1_plus = L_1_plus, L_2 = L_2, x = x, rho = rho, mu_tilde = mu_tilde, sigma_tilde = sigma_tilde, c_tilde = c_tilde, M = M) #Named tuple for parameters.
-
-        #Check upwind direction
-        @assert minimum(mu_tilde.(T, x)) >= 0.0
-        @assert minimum(mu_tilde.(0.0, x)) >= 0.0
-
-        #Calculating the stationary solution,
-        L_T = rho*I - Diagonal(mu_tilde.(T, x)) * L_1_plus - Diagonal(sigma_tilde.(T, x).^2/2.0) * L_2
-        u_T = L_T \ c_tilde.(T, x)
-
-        u_ex_T = [u_T; 1.0] #Closed form for the trivial linear function we are adding
-
-        @assert(issorted(u_T)) #We are only solving versions that are increasing for now
+    @assert(issorted(u_T)) #We are only solving versions that are increasing for now
 
 
-        function f(resid,du_ex,u_ex,p,t)
-            L = (p.rho*I  - Diagonal(p.mu_tilde.(t, x)) * p.L_1_plus - Diagonal(p.sigma_tilde.(t, p.x).^2/2.0) * p.L_2)
-            u = u_ex[1:p.M]
-            resid[1:M] .= L * u_ex[1:p.M] - p.c_tilde.(t, p.x)
-            resid[M+1] .= u_ex[M+1] - 1.0
-            resid .-= du_ex
-        end
-
-        #Should Verifying that the initial condition is solid
-        resid_T = zeros(u_ex_T) #preallocation
-        du_ex_T = zeros(u_ex_T)
-        f(resid_T, du_ex_T, u_ex_T, p, T)
-        @show norm(resid_T)
-        @assert norm(resid_T) < 1.0E-10
-
-        tspan = (T, 0.0)
-        return DAEProblem(f, zeros(u_ex_T), u_ex_T, tspan, differential_vars = [trues(u_T); false], p)
+    function f(resid,du_ex,u_ex,p,t)
+        L = (p.rho*I  - Diagonal(p.mu_tilde.(t, x)) * p.L_1_plus - Diagonal(p.sigma_tilde.(t, p.x).^2/2.0) * p.L_2)
+        u = u_ex[1:p.M]
+        resid[1:M] .= L * u_ex[1:p.M] - p.c_tilde.(t, p.x)
+        resid[M+1] .= u_ex[M+1] - 1.0
+        resid .-= du_ex
     end
 
-    probDAE = createDAEproblem(c_tilde, sigma_tilde, mu_tilde, x_min, x_max, M, T, rho)
-    solDAE = solve(probDAE, IDA())
-    plot(solDAE, vars=1:plotevery:M)
-    @show(issorted(solDAE[end][1:M]))
+    #Should Verifying that the initial condition is solid
+    resid_T = zeros(u_ex_T) #preallocation
+    du_ex_T = zeros(u_ex_T)
+    f(resid_T, du_ex_T, u_ex_T, p, T)
+    @show norm(resid_T)
+    @assert norm(resid_T) < 1.0E-10
 
-    # @benchmark solve($probDAE, IDA())
-
-    #Check they are "reasonably" close
-    @show norm(sol[1] - solDAE[1][1:M])
-    @show norm(sol[end] - solDAE[end][1:M])
+    tspan = (T, 0.0)
+    return DAEProblem(f, zeros(u_ex_T), u_ex_T, tspan, differential_vars = [trues(u_T); false], p)
 end
+
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,35 @@
-using PerlaTonettiWaugh
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
-end
+using PerlaTonettiWaugh, Plots, BenchmarkTools, Sundials, Base.Test
 
-# write your own tests here
-@test 1 == 2
+# Testing code
+x_min = 0.01
+x_max = 1.0
+M = 20
+T = 10.0
+rho = 0.15
+sigma_bar = 0.1
+c_tilde(t, x) = x + 0.0001*t
+sigma_tilde(t, x) =  sigma_bar
+mu_tilde(t, x) = 0.1*x *(1.0 + 4.0 * t)
+basealgorithm = CVODE_BDF() #CVODE_CDF(linear_solver=:GMRES) #ImplicitEuler() #A reasonable alternative. Algorithms which don't work well: Rosenbrock23(), Rodas4(), KenCarp4()
+plotevery = 5
+
+prob = createODEproblem(c_tilde, sigma_tilde, mu_tilde, x_min, x_max, M, T, rho)
+sol = solve(prob, basealgorithm)
+plot(sol, vars=1:plotevery:M)
+@assert(issorted(sol[end]))
+# @benchmark solve($prob, $basealgorithm) #Benchmark
+
+#Solve backwards with a DAE and a trivial algebraic equation
+#u_ex(1:M) = u(), following the ODE
+#u_ex(M+1) = 1.0 #(i.e., a trivial linear function)
+
+probDAE = createDAEproblem(c_tilde, sigma_tilde, mu_tilde, x_min, x_max, M, T, rho)
+solDAE = solve(probDAE, IDA())
+plot(solDAE, vars=1:plotevery:M)
+@show(issorted(solDAE[end][1:M]))
+
+# @benchmark solve($probDAE, IDA())
+
+#Check they are "reasonably" close
+@show norm(sol[1] - solDAE[1][1:M])
+@show norm(sol[end] - solDAE[end][1:M])
