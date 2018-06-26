a39791bf1271dbdfa30077e3862b5044793bd941
Arnav Sood <soodarnav01@gmail.com>
2018-06-26 13:33:11 -0700

    rename

--- test/simple/algebraicstationarytest.jl
+++ test/simple/algebraicstationarytest.jl
@@ -24,10 +24,10 @@ macro with_kw(args...)
 end
 
 # Generate default parameters. 
-params = @with_kw(γ = 0.005, σ = 0.02, α = 2.1, r = 0.05, ζ = 14.5)
+simple_algebraic_params = @with_kw(γ = 0.005, σ = 0.02, α = 2.1, r = 0.05, ζ = 14.5)
 
 # Test them
-results = stationary_algebraic(params());
+results = stationary_algebraic_simple(simple_algebraic_params());
 @test results.g ≈ 0.0211182826;
 @test results.υ ≈ 1.75369955156;
 @test results.v(0) ≈ 35.04962283;
