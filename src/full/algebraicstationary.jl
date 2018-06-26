a1402f8cc71a44cc42051d7017a5ce5153ac803a
Arnav Sood <soodarnav01@gmail.com>
2018-06-26 16:34:03 -0700

    moer equations for full algebraic

--- src/full/algebraicstationary.jl
+++ src/full/algebraicstationary.jl
@@ -11,8 +11,15 @@ function stationary_algebraic_full(params)
 
     # Define auxiliary objects.
     F(z) = 1 - z^(-θ); # (H.1)
-    S(g) = θ * (g - μ - θ * v^2/2); # (H.2)
-    r(g) = ρ + γ*g + δ; # (H.6)
+    S = θ * (g - μ - θ * v^2/2); # (H.2)
+    ν = (μ - g)/v^2 + √(((g-μ)/(v^2))^2 + (r-g)/(v^2/2)); # (H.3)
+    a = (r - g - (σ - 1)*(μ - g - (σ-1)v^2/2))^(-1); # (H.4)
+    b = (1 - a*(r-g))*d^(1-σ)*z^(ν + σ - 1); # (H.5)
+    r = ρ + γ*g + δ; # (H.6)
+    L = Ω*((N-1)*(1-F(̂z))*κ + (1-η)*ζ*(S + δ/χ))
+    # (H.8)
+    ̂z = d * (κ/π)^(1/(σ-1)); # (H.9)
+    w = (̄σ)^(-1) * ̄z; # (H.10)
     x = ζ * (1 - η + η * Θ/w); # (H.11)
     # Validate auxiliary objects. 
     # Define equilibrium equations. 
