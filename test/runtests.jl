013fe28680db6d0655670b445bfc140046940058
Arnav Sood <soodarnav01@gmail.com>
2018-06-28 13:17:47 -0700

    add to test set

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,6 @@ using PerlaTonettiWaugh, Base.Test
 tic()
 @time @testset "Analytical Simple Steady State" begin include("simple/algebraicstationarytest.jl") end
 @time @testset "Numerical Simple Steady State" begin include("simple/numericalsteadystatetest.jl") end
+@time @testset "Regression Simple Steady State" begin include("simple/regressiontest.jl") end 
 @time @testset "Analytical Full Steady State" begin include("full/algebraicstationarytest.jl") end
 toc()
