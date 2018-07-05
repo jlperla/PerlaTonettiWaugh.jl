8530879c9737b0272ce2294b65c7385608a159c4
Arnav Sood <soodarnav01@gmail.com>
2018-07-05 15:28:39 -0700

    add algebraic stationary implementation

--- src/full/algebraicstationary.jl
+++ src/full/algebraicstationary.jl
@@ -1,31 +1,42 @@
-# #Implements the algebraic stationary solution from the full model, returning back [WHAT IT RETURNS].
+# Implements the algebraic stationary solution for the full model. Returns the equilibrium quantities (g, Ω, π) determined by equations H.15-H.17.
 
-# using Parameters, NamedTuples
+# Dependencies. 
+using Parameters, NLsolve
+include("fullparams.jl")
 
-# function stationary_algebraic_full(params)
-#     # Unpack parameters.
-#     @unpack ρ, σ, n, θ, γ, d, κ, ζ, η, Θ, χ, υ, μ, δ = params
+"""
+Function to return the residuals for the equilibrium equations H.15-H.17 in-place, given the values of (g, Ω, π). 
+    
+function f!(F, x)
+"""
+function f!(G, x)
+    # Unpack parameters and inputs. 
+    @unpack ρ, σ, N, θ, γ, d, κ, ζ, η, Theta, χ, υ, μ, δ = fullparams()
+    g = x[1]
+    Ω = x[2]
+    π = x[3]
 
-#     # Validate parameters.
-#     @assert υ > 0 # To avoid discontinuity, by assumption.
+    # Compute interim quantities.
+    F(z) = 1 - z^(-θ) # H.1 
+    z_hat = d*(κ/π)^(inv(σ-1)) # H.9
+    r = ρ + γ*g + δ # H.6 
+    ν = (μ-g)/υ^2 + sqrt(((g-μ)/υ^2)^2 + (r-g)/(υ^2/2)) # H.3
+    a = inv(r - g - (σ - 1)*(μ - g + (σ - 1)*υ^2/2)) # H.4 
+    b = (1 - a*(r-g))*d^(1-σ)*z_hat^(ν + σ - 1) # H.5 
+    S = θ * (g - μ - θ * υ^2/2) # H.2   
+    L_tilde = Ω * ((N-1)*(1-F(z_hat))*κ + (1-η)*ζ*(S + δ/χ)) # H.7
+    z_bar = (Ω * (θ)/(1 + θ - σ) + (N-1)*(1-F(z_hat))*d^(1-σ)*(z_hat^(-1 + σ)*θ/(1 + θ - σ))) # H.8
+    w = inv(σ)*z_bar # H.10
+    x = ζ * (1- η + η * Theta / w) # H.11
 
-#     # Define auxiliary objects.
-#     F(z) = 1 - z^(-θ); # (H.1)
-#     S = θ * (g - μ - θ * v^2/2); # (H.2)
-#     ν = (μ - g)/v^2 + √(((g-μ)/(v^2))^2 + (r-g)/(v^2/2)); # (H.3)
-#     a = (r - g - (σ - 1)*(μ - g - (σ-1)v^2/2))^(-1); # (H.4)
-#     b = (1 - a*(r-g))*d^(1-σ)*z^(ν + σ - 1); # (H.5)
-#     r = ρ + γ*g + δ; # (H.6)
-#     L = Ω*((N-1)*(1-F(z))*κ + (1-η)*ζ*(S + δ/χ))
-#     # (H.8)
-#     ̂z = d * (κ/π)^(1/(σ-1)); # (H.9)
-#     w = (̄σ)^(-1) * ̄z; # (H.10)
-#     x = ζ * (1 - η + η * Θ/w); # (H.11)
-#     # Validate auxiliary objects. 
-#     # Define equilibrium equations. 
-#     # Solve equilibrium equations. 
-#     # Validate equilibrium results.
-#     @assert g*(1-γ) < ρ # To ensure finite utility; see Appendix D.  
-#     # Return results. 
-#     return true
-# end 
+    # Calculate and assign residuals. 
+    G[1] = x/π - a*(χ/(1-χ))*(σ + ν - 1)/(ν) # H.15
+        # Calculate H.16 intermediates. 
+        big_denom = ν*(θ + ν)*(θ - σ + 1) # Part of H.16
+        denom_1 = a*(g - r) # Part of H.16
+        num_1 = ν*(N-1)*(θ - σ + 1)*(d^(1 - σ)*(θ + ν)*z_hat^(-θ + σ - 1)-b*θ*z_hat^(-θ-ν)) # Part of H.16
+        num_2 = θ*(ν*(N-1)*d^(1-σ)*(θ+ν)*z_hat^(-θ + σ -1) + (ν + σ - 1)*(θ + ν - σ + 1)) # Part of H.16
+    G[2] = 1 + (σ-1)/ν - (num_1/denom_1 + num_2)/big_denom - (χ/(1-χ))*(σ + ν - 1)/(ν) # H.16
+    G[3] = π - (1- L_tilde)/((σ -1)*z_bar^(σ-1)) # H.17
+end
+
---
+++ src/full/fullparams.jl
@@ -0,0 +1,5 @@
+# These params come from ~/blob/master/code/calculate_equilibrium_matlab/gbm_free_entry/default_equilibrium_values.m
+
+# Except for υ, which we make low and positive. 
+
+fullparams = @with_kw(ρ = 0.05, σ = 3, N = 10, θ = 3.22, γ = 0, d = 5.49, κ = 0.06, ζ = 1.9, η = 1, Theta = 1, χ = 1/3, υ = 0.05, μ = 0, δ = 0)
