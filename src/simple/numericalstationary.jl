1fcbed60838f420fee1d5f7902d3a51e3981e92d
stevenzhangdx <stevenzhangdx@gmail.com>
2018-07-10 11:00:19 -0700

    Added simple numerical stationary

--- src/simple/numericalstationary.jl
+++ src/simple/numericalstationary.jl
@@ -1,1 +1,29 @@
 # Numerically solve for the stationary solution as a system of equatoins.  
+using BandedMatrices, NamedTuples, Roots, Parameters
+
+function stationary_numerical_simple(params, z)
+    M = length(z)
+    # Unpack parameters. 
+    @unpack γ, σ, α, r, ζ = params
+    function stationary_numerical_given_g(g)
+        x, L_1_plus, L_2  = irregulardiffusionoperators(z, M) #Discretize the operator
+        L_T = (r - g)*I - Diagonal(γ - g) * L_1_plus - Diagonal(σ^2/2.0) * L_2 
+        π = exp(z)
+        v_T = L_T \ π
+        f_z = α * exp(-α*z)
+        ω = f_z ./ sum(f_z)
+
+        diff = v_T[1] + ζ - ω * v_T
+        return diff
+    end
+
+f(g) =  stationary_numerical_given_g(g)
+g_T = find_zero(f, (0.0001, 2))
+
+@unpack γ, σ, α, r, ζ = params
+        x, L_1_plus, L_2  = irregulardiffusionoperators(z, M) #Discretize the operator
+        L_T = (r - g_T)*I - Diagonal(γ - g_T) * L_1_plus - Diagonal(σ^2/2.0) * L_2 
+        π = exp(z)
+        v_T = L_T \ π
+    return v_T
+end
--- test/simple/numericalsteadystatetest.jl
+++ test/simple/numericalsteadystatetest.jl
@@ -1,3 +1,24 @@
-using PerlaTonettiWaugh, Base.Test
+using PerlaTonettiWaugh, Base.Test, Parameters, NamedTuples
 
-@test 1==1
+# Generate default parameters. 
+
+simple_numerical_params = @kw_nt(γ = 0.005, σ = 0.02, α = 2.1, r = 0.05, ζ = 14.5)
+
+
+z_min = 0
+z_max = 5
+M = 100
+z = linspace(z_min, z_max, M);
+temp = (rand(M) - 0.5)/100;
+temp[1] = 0
+temp[end] = 0
+z = z + temp;
+
+# Test
+results = stationary_numerical_simple(simple_numerical_params(), z)
+
+@test results.g ≈ 0.0211182826;
+@test results.ν ≈ 1.75369955156;
+@test results.v(0) ≈ 35.04962283;
+@test results.v(2) ≈ 165.31581267;
+@test results.v(5) ≈ 3312.7957099;
