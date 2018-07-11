2f1ee04fcb03fdb15262ed956dd00915ff63a766
Arnav Sood <soodarnav01@gmail.com>
2018-07-11 15:13:50 -0700

    add suppressor for test clarity

--- test/full/algebraicstationarytest.jl
+++ test/full/algebraicstationarytest.jl
@@ -1,4 +1,4 @@
-using PerlaTonettiWaugh, Base.Test, NLsolve, BenchmarkTools
+@suppress using PerlaTonettiWaugh, Base.Test, NLsolve, BenchmarkTools
 include("../../src/full/fullparams.jl")
 
 # Run a basic test. (neglecting nlsolve stability)
