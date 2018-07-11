6f35c0230c8db1d905595c205e76b261a7983dba
Sev <ch38984@mail.ubc.ca>
2018-07-10 17:14:17 -0700

    correction 3

--- src/irregulardiffusionoperators.jl
+++ src/irregulardiffusionoperators.jl
@@ -14,13 +14,13 @@ function irregulardiffusionoperators(x, M)
     dl_p1 = zeros(M-1)./Δ_m[2:end] # This is flipped delta vector as the tridiagnal matrix is flipped? not sure
     d_p1 = -1 * ones(M)./Δ_p
     d_p1[end] = 0
-    du_p1 = ones(M-1)./Δ_p[2:end]
+    du_p1 = ones(M-1)./Δ_p[1:end-1]
     L_1_plus = Tridiagonal(dl_p1, d_p1, du_p1)
 
-    dl_m1 = ones(M-1)./Δ_m[2:end]
+    dl_m1 = -ones(M-1)./Δ_m[2:end]
     d_m1 = ones(M)./Δ_m
     d_m1[1] = 0
-    du_m1 = zeros(M-1)./Δ_m[2:end]
+    du_m1 = zeros(M-1)./Δ_p[1:end-1]
     L_1_minus = Tridiagonal(dl_m1, d_m1, du_m1)
 
     Δ=Δ_p+Δ_m
@@ -29,7 +29,7 @@ function irregulardiffusionoperators(x, M)
     d_2[1] = -1
     d_2[end] = -1
     d_2 = d_2./(Δ_p.*Δ_m)
-    du_2 = 2*ones(M-1)./(Δ_p[2:end].*Δ[2:end])
+    du_2 = 2*ones(M-1)./(Δ_p[1:end-1].*Δ[1:end-1])
     L_2 = Tridiagonal(dl_2, d_2, du_2)
 
     #BandedMatrix are much faster, probably because of better specializations in the composition
