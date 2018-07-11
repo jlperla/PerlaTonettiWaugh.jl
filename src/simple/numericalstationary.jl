0373163003fa9dee488450447bf9a55dfe764945
stevenzhangdx <stevenzhangdx@gmail.com>
2018-07-11 10:37:33 -0700

    update numerical steady state solver

--- src/simple/numericalstationary.jl
+++ src/simple/numericalstationary.jl
@@ -5,22 +5,35 @@ function stationary_numerical_simple(params, z)
     M = length(z)
     # Unpack parameters. 
     @unpack γ, σ, α, r, ζ = params
+    
     function stationary_numerical_given_g(g)
-        x, L_1_plus, L_2  = irregulardiffusionoperators(z, M) #Discretize the operator
-        L_T = (r - g)*I - (γ - g) * L_1_plus - (σ^2/2.0)* L_2 
+        x, L_1_minus, L_1_plus, L_2  = irregulardiffusionoperators(z, M) #Discretize the operator
+        if γ - g < 0
+            L_1 = L_1_minus
+        else
+            L_1 = L_1_plus
+        end
+        L_T = (r - g)*I - (γ - g) * L_1 - (σ^2/2.0)* L_2 
         π = exp.(z)
         v_T = L_T \ π
         f_z = α * exp.(-α*z)
-        ω = f_z ./ sum(f_z)
+        f_i = f_z ./ (1 - exp.(-α*z[end]))
+        ω = f_i ./ sum(f_i)
+
 
         diff = v_T[1] + ζ - ω' * v_T
         return diff
     end
 
-    g_T = find_zero(stationary_numerical_given_g, (0.0001, 2))
+    g_T = find_zero(stationary_numerical_given_g, (-0.0001, 0.5))
 
-    x, L_1_plus, L_2  = irregulardiffusionoperators(z, M) #Discretize the operator
-    L_T = (r - g_T)*I - (γ - g_T) * L_1_plus - (σ^2/2.0) * L_2
+    x, L_1_minus, L_1_plus, L_2  = irregulardiffusionoperators(z, M) #Discretize the operator
+    if γ - g_T < 0
+        L_1 = L_1_minus
+    else
+        L_1 = L_1_plus
+    end
+    L_T = (r - g_T)*I - (γ - g_T) * L_1 - (σ^2/2.0) * L_2
     π = exp.(z)
     v_T = L_T \ π
     ν_T = (γ-g_T)/σ^2 + √(((g_T-γ)/σ^2)^2 + (r-g_T)/(σ^2/2));
