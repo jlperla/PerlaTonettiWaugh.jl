9f5b8af4f3ff0304b2cd24bc0273793cefe71e6d
Sev <ch38984@mail.ubc.ca>
2018-07-17 11:26:45 -0700

    test with analytic

--- src/simple/simple_dynamic_ODE.jl
+++ src/simple/simple_dynamic_ODE.jl
@@ -1,12 +1,13 @@
 # This function generate ODE problem with r,π and ζ and g time varying
 function create_dynamic_ODE(params,settings)
     # Unpacking
-    gamma=params.gamma;
-    sigma=params.sigma;
+    γ=params.γ;
+    σ=params.σ;
     #alpha=params.alpha; # used for distribution in value matching
     ζ=params.ζ;
     π=params.π;
     r=params.r;
+    α=params.α; 
 
     # unpacking settings
     z=settings.z;
@@ -15,7 +16,7 @@ function create_dynamic_ODE(params,settings)
     g=settings.g;
     z, L_1_minus, L_1_plus, L_2  = irregulardiffusionoperators(z, M); #Discretize the operator
     
-    mu_tilde(t,x)=gamma.-g(t,x);
+    mu_tilde(t,x)=γ.-g(t,x);
     rho_p(t,x)=r(t,x).-g(t,x);
 
         #Check upwind direction
@@ -28,22 +29,22 @@ function create_dynamic_ODE(params,settings)
             error("Not weakly positive or negative") # Not strictly necessary, but good to have redundancy here.
         end
     #Calculating the stationary solution,
-    L_T = Diagonal(rho_p(T,z)) - Diagonal(mu_tilde.(T, z)) * p.L_1 - Diagonal(sigma.(T, z).^2/2.0) * L_2
-    u_T = L_T \ π.(T, z)
-    @assert(issorted(u_T))
+    L_T = Diagonal(rho_p(T,z)) - Diagonal(mu_tilde.(T, z)) * p.L_1 - Diagonal(σ.^2/2.0) * L_2
+    v_T = L_T \ π.(T, z)
+    @assert(issorted(v_T))
 
     function f(du,u,p,t)
-        L = (Diagonal(rho_p(T,z)) - Diagonal(p.mu_tilde.(t, z)) * p.L_1 - Diagonal(p.sigma.(t, p.z).^2/2.0) * p.L_2)
+        L = (Diagonal(rho_p(T,z)) - Diagonal(p.mu_tilde.(t, z)) * p.L_1 - Diagonal(σ.^2/2.0) * p.L_2)
         A_mul_B!(du,L,u)
         du .-= p.π.(t, p.z)
     end
 
     #Checks on the residual
-    du_T = zeros(u_T)
-    f(du_T, u_T, p, T)
-    @show norm(du_T)
-    @assert norm(du_T) < 1.0E-10
+    dv_T = zeros(v_T)
+    f(dv_T, v_T, p, T)
+    @show norm(dv_T)
+    @assert norm(dv_T) < 1.0E-10
 
-    return ODEProblem(f, u_T, (T, 0.0), p)
+    return ODEProblem(f, v_T, (T, 0.0), p)
 
 end
--- test/simple/test_simple_dynamic_code.jl
+++ test/simple/test_simple_dynamic_code.jl
@@ -1,22 +1,33 @@
 # a simple test of simple_dynamic_ODE.jl
 
-using PerlaTonettiWaugh, Plots, BenchmarkTools, Sundials, Base.Test,Interpolations, QuantEcon
+using PerlaTonettiWaugh, Plots, BenchmarkTools, Sundials, Base.Test,Interpolations, QuantEcon, NamedTuples
 solve(x,y)=Sundials.solve(x,y)
-# Testing code
+# setting up
 z_min = 0.01
-z_max = 1.0
-M = 20
+z_max = 5.0
+M = 100
 z=linspace(z_min,z_max,M)
 T = 10.0
-sigma_bar = 0.1
-π(t, x) = exp(x)*(1.0+0.0*t)
-sigma(t, x) =  sigma_bar
-g(t, x) = 0.02 + 0.0*x + 0.0*t
-ζ(t, x) = 14.5 + 0.0*x + 0.0*t
-r(t, x) = 0.05 + 0.0*x + 0.0*t
-gamma=0.005;
+π(t, x) = exp(x)*(1.0+0.0*t);
+σ = 0.02
+α = 2.1
+ζ_n = 14.5
+r_n = 0.05
+ζ(t, x) = ζ_n + 0.0*x + 0.0*t
+r(t, x) = r_n + 0.0*x + 0.0*t
+γ=0.005;
 
-params=@NT(gamma=gamma, sigma=sigma, π=π, ζ=ζ, r = r)
+# solve for numerical g_T as test
+params_n=@kw_nt(γ=0.005, σ=0.02, α=2.1, r=0.05, ζ=14.5)
+
+res=stationary_numerical_simple(params_n(),z)
+g_analytic=res.g
+v_analytic=res.v
+@show v_analytic
+
+g(t, x) = g_analytic + 0.0*x + 0.0*t;
+
+params=@NT(γ=γ, σ=σ, π=π, ζ=ζ, r = r, α=α)
 settings=@NT(z = z,g = g, T = T)
 basealgorithm = CVODE_BDF() #CVODE_CDF(linear_solver=:GMRES) #ImplicitEuler() #A reasonable alternative. Algorithms which don't work well: Rosenbrock23(), Rodas4(), KenCarp4()
 plotevery = 5
@@ -25,3 +36,7 @@ prob = create_dynamic_ODE(params,settings)
 sol = solve(prob, basealgorithm)
 # plot(sol, vars=1:plotevery:M)
 @test issorted(sol[end])
+@show sol[end]
+
+# test whether ODE result close to analytic
+
