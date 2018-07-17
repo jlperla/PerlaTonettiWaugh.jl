39a6bc3cc02013bd4fbe5db919eaf48c70d349f4
Sev <ch38984@mail.ubc.ca>
2018-07-17 15:56:01 -0700

    test case1

--- test/simple/test_simple_dynamic_code.jl
+++ test/simple/test_simple_dynamic_code.jl
@@ -5,7 +5,7 @@ solve(x,y)=Sundials.solve(x,y)
 # setting up
 z_min = 0.0
 z_max = 5.0
-M = 100
+M = 500
 z=linspace(z_min,z_max,M)
 T = 10.0
 π(t, x) = exp(x)*(1.0+0.0*t);
@@ -38,7 +38,7 @@ prob = create_dynamic_ODE(params,settings)
 sol = solve(prob, basealgorithm)
 # plot(sol, vars=1:plotevery:M)
 @test issorted(sol[end])
-@show sol[end]
+#@show sol[end]
 
 
 # 2. test whether ODE result close to analytic
@@ -53,22 +53,34 @@ settings_g=@NT(z = z,g = g_g, T = T)
 
 prob_g = create_dynamic_ODE(params,settings_g)
 sol_g = solve(prob_g, basealgorithm)
-@show sol_g[end]
+#@show sol_g[end]
 @test issorted(sol_g[end])
 
 # 3. Test non uniform grid z
 
-z_add=linspace(z_min,z_max,37)
-z_comb=unique(sort([z;z_add]))
+z_comb= unique([linspace(z_min, 1.0, 500)' linspace(1.0, z_max, 201)'])
 
 settings_ir=@NT(z = z_comb,g = g, T = T)
 
 prob_ir = create_dynamic_ODE(params,settings_ir)
 sol_ir = solve(prob_ir, basealgorithm)
-@show sol_ir[end]
+#@show sol_ir[end]
 @test issorted(sol_ir[end])
 
 # interpolate uniform onto non uniform grid
 sol_int=LinInterp(z, sol[end])
 @show norm(sol_int.(z_comb)-sol_ir[end])
-@show norm(sol_ir[end,end]-sol[end,end])<1e-2
+@show norm(sol_ir[end,end]-sol[end,end])
+
+# If comparing with numerical using z_comb grid (g_analytic will be different)
+res_comb=stationary_numerical_simple(params_n(),z_comb)
+g_acomb=res_comb.g
+v_acomb=res_comb.v
+g_comb(t, x) = g_acomb + 0.0*x + 0.0*t;
+
+settings_ir2=@NT(z = z_comb,g = g_comb, T = T)
+
+prob_ir2 = create_dynamic_ODE(params,settings_ir2)
+sol_ir2 = solve(prob_ir2, basealgorithm)
+
+@show norm(v_acomb-sol_ir2[end])
