1d948e5de6e8aa2a7e9f4ad9095df88d50f287ea
Sev <ch38984@mail.ubc.ca>
2018-08-02 11:14:20 -0700

    rescaling

--- src/simple/numericalstationary.jl
+++ src/simple/numericalstationary.jl
@@ -2,20 +2,22 @@
 function stationary_numerical_simple(params, z)
     M = length(z)
     # Unpack parameters. 
-    @unpack γ, σ, α, r, ζ = params
+    @unpack γ, σ, α, r, ζ, ξ = params
 
     # Define the pdf of the truncated exponential distribution
     ourDist = Truncated(Exponential(1/α), z[1], z[end])
     ω = irregulartrapezoidweights(z, ourDist)
 
     function stationary_numerical_given_g(g)
-        z, L_1_minus, L_1_plus, L_2  = diffusionoperators(z) #Discretize the operator
+        z, L_1_minus, L_1_plus, L_2  = rescaled_diffusionoperators(z, ξ) #Discretize the operator
 
-        L_T = (r - g)*I - (γ - g) * L_1_minus - (σ^2/2.0)* L_2 
-        π = exp.(z)
-        v_T = L_T \ π
+        r_tild = r - g - ξ*(γ - g) - σ^2/2*ξ^2;
+        μ_tild = γ - g + σ^2*ξ;
+        π_tild(z) = 1;
+        L_T = r_tild*I - μ_tild*L_1_minus - σ^2/2 * L_2 # Construct the aggregate operator. 
+        v_T = L_T \ π_tild(z) # Solution to the rescaled differential equation. 
 
-        diff = v_T[1] + ζ - dot(ω, v_T)
+        diff = v_T[1] + ζ - dot(ω, exp.(ξ.*z).*v_T)
         return diff
     end
 
@@ -25,9 +27,11 @@ function stationary_numerical_simple(params, z)
     @assert(γ - g_T < 0)  # Error if γ - g is positive
 
     # Recreate what the ODE returned for the value function. 
-    z, L_1_minus, L_1_plus, L_2  = diffusionoperators(z) #Discretize the operator
-    L_T = (r - g_T)*I - (γ - g_T) * L_1_minus - (σ^2/2.0) * L_2
-    π = exp.(z)
-    v_T = L_T \ π 
+    z, L_1_minus, L_1_plus, L_2  = rescaled_diffusionoperators(z, ξ) #Discretize the operator
+    r_tild = r - g_T - ξ*(γ - g_T) - σ^2/2*ξ^2;
+    μ_tild = γ - g_T + σ^2*ξ;
+    π_tild(z) = 1;
+    L_T = r_tild*I - μ_tild*L_1_minus - σ^2/2 * L_2 # Construct the aggregate operator. 
+    v_T = L_T \ π_tild(z) # Solution to the rescaled differential equation. 
     return @NT(g = g_T, v = v_T)
 end 
