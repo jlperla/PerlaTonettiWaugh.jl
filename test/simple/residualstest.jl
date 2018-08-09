8d04536ec2bf239b41051c5e38e837aca369a137
Chiyoung Ahn <chiyahn@hotmail.com>
2018-08-09 14:46:50 -0700

    added a few more tests with time-varying `\pi (t)` and `r(t)`

--- test/simple/residualstest.jl
+++ test/simple/residualstest.jl
@@ -10,11 +10,6 @@
     N = 10
     t = linspace(0.0, T_val, N) # For interpolation purposes only. 
 
-    # Functional parameters. 
-    π_tilde_func = (t, z) -> 1 # Potentially idiosyncratic. 
-    x_func = t -> ζ_val # Not idiosyncratic, per equation (4)
-    r_func = t -> r_val # Not idiosyncratic, per intro to doc. 
-
     # Constant parameters. 
     σ_val = 0.02
     α_val = 2.1
@@ -23,12 +18,29 @@
     γ_val = 0.005
     ξ_val = 1.0
     
+    # Functional parameters. 
+    x_func = t -> ζ_val # Not idiosyncratic, per equation (4)
+
+    r_vector = r_val + 1e-04 * (1 - t / T_val)
+    r_int = LinInterp(t, r_vector)
+    r_func_varying = t -> r_int(t) # Not idiosyncratic, per intro to doc.    
+    r_func_const = t -> r_val
+
+    π_tilde_vector = 1 + 1e-04 * (1 - t / T_val)
+    π_tilde_int = LinInterp(t, π_tilde_vector)
+    π_tilde_func_varying = (t, z) -> π_tilde_int(t) # Not idiosyncratic, per intro to doc.    
+    π_tilde_func_const = (t, z) -> 1 # Potentially idiosyncratic. 
+    
     # Param generators and param NTs. 
     params_const = @with_kw (γ = γ_val, σ = σ_val, α = α_val, r = r_val, ζ = ζ_val, ξ = ξ_val, π_tilde = z -> 1) 
-    params_func = @with_kw (γ = γ_val, σ = σ_val, α = α_val, r = r_func, x = x_func, ξ = ξ_val, π_tilde = π_tilde_func)
+    params_func = @with_kw (γ = γ_val, σ = σ_val, α = α_val, r = r_func_const, x = x_func, ξ = ξ_val, π_tilde = π_tilde_func_const)
     params_const = params_const()
-    params_func = params_func()
- 
+    params_func_const = params_func()
+    params_func_varying_1 = params_func(π_tilde = π_tilde_func_varying)
+    params_func_varying_2 = params_func(r = r_func_varying)
+    params_func_varying_3 = params_func(r = r_func_varying, π_tilde = π_tilde_func_varying)
+
+    
     # Solutions.
     # Solve for the numerical stationary g_T. 
     result_ns = stationary_numerical_simple(params_const, z_grid)
@@ -43,9 +55,17 @@
     settings = @with_kw (z = z_grid, T = T_val, g = g_func, ode_solve_algorithm = CVODE_BDF())
 
     # Solve for v with time-varying g
-    resid1 = calculate_residuals(params_func, settings())
+    resid = calculate_residuals(params_func_const, settings())
     @test_broken norm(resid) ≈ 0 atol = 1e-10 # since time-varying g is not in equilibrium, we expect broken at this moment
 
     # Test the stationary residual is close to zero.
-    resid2 = calculate_residuals(params_func, settings(g = t -> g_stationary))
-    @test norm(resid2) ≈ 0 atol = 1e-10
+    resid = calculate_residuals(params_func_const, settings(g = t -> g_stationary))
+    @test norm(resid) ≈ 0 atol = 1e-10
+
+    # Solve with time-varying r and π_tilde (since g is not in equilibrium, we expect broken at this moment)
+    resid = calculate_residuals(params_func_varying_1, settings(g = t -> g_stationary))
+    @test_broken norm(resid) ≈ 0 atol = 1e-10
+    resid = calculate_residuals(params_func_varying_2, settings(g = t -> g_stationary))
+    @test_broken norm(resid) ≈ 0 atol = 1e-10
+    resid = calculate_residuals(params_func_varying_3, settings(g = t -> g_stationary))
+    @test_broken norm(resid) ≈ 0 atol = 1e-10
