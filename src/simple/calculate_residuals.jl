433ce018e65bc2345e6430e024358a36295cbe03
Chiyoung Ahn <chiyahn@hotmail.com>
2018-08-10 16:24:43 -0700

    changes in calculate_residuals

--- src/simple/calculate_residuals.jl
+++ src/simple/calculate_residuals.jl
@@ -1,4 +1,4 @@
-# This function calculatesthe residual at all points given on g.
+# This function calculates the residual at all points given on g.
 function calculate_residuals(params, settings) # To keep the params consistent with other tuples. 
     # Setup
     @unpack γ, σ, α, r, x, ξ, π_tilde = params
@@ -8,14 +8,20 @@ function calculate_residuals(params, settings) # To keep the params consistent w
     ω = ω_weights(z, α, ξ)
 
     # Define and solve dynamic ODE. 
-    prob = simpleODE(params, settings)
-    sol = Sundials.solve(prob, ode_solve_algorithm)
+    ode_prob = simpleODE(params, settings)
+    
+    return calculate_residuals(ode_prob, x, ω, ode_solve_algorithm)
+end
+
+function calculate_residuals(ode_prob, x, ω, ode_solve_algorithm) # To keep the params consistent with other tuples. 
+    # Solve ode
+    sol = Sundials.solve(ode_prob, ode_solve_algorithm)
     t_vals = sol.t
 
     # Calculate the residual at each time point
     residuals = zeros(length(t_vals))
     for (i, t) in enumerate(t_vals)
-        v_t = sol(t)    #i.e., the value function at the point.
+        v_t = sol(t) # i.e., the value function at the point.
         residuals[i] = v_t[1] + x(t) - dot(ω, v_t)
     end
     
