5cb5d28c6f3a626554c8898e3a0df74ce46082eb
Chiyoung Ahn <chiyahn@hotmail.com>
2018-08-23 13:21:46 -0700

    remove print(z_hat_t) for testing
    [ci skip]

--- src/full/dynamic.jl
+++ src/full/dynamic.jl
@@ -79,9 +79,6 @@ end
 function get_L_tilde_t(p, t, g_t, z_hat_t)
     @unpack N, T, θ, κ, ζ, δ, χ, Ω = p
     is_t_over_T = ifelse(t >= T, 1, 0)
-    if (t < T)
-    print(string(z_hat_t, ", θ*κ: ", θ*κ, ", t: ", t, "\n")) # TODO: remove this
-    end
     return Ω(t) * ((N-1) * z_hat_t^(-θ * κ) + ζ*θ*g_t + is_t_over_T * ζ * δ / χ)
 end
 
