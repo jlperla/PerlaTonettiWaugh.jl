643663afb3d0d5a8a7d9f0902d2947f78f4f1d31
Jesse Perla <jesseperla@gmail.com>
2018-09-14 13:32:09 -0700

    Passes basck the `f!` and has a jupyter notebook to test and explore it

--- src/full/dynamic.jl
+++ src/full/dynamic.jl
@@ -1,12 +1,12 @@
 function solve_dynamics(params_T, stationary_sol_T, settings, T, Ω)
     @unpack δ, N, σ, θ, d = params_T
-    @unpack z, tstops, Δ_E = settings 
+    @unpack z, tstops, Δ_E = settings
     @assert params_T.γ ≈ 1 # γ has to be close 1 to have consistent results with the stationary solutions
     M = length(z)
 
     # define E(t) based on FD
     E(t) = (log(Ω(t+Δ_E)) - log(Ω(t-Δ_E)))/(2*Δ_E) + δ
-    
+
     # define the corresponding DAE problem
     p = get_p(params_T, stationary_sol_T, settings, Ω, T)
     dae = PTW_DAEProblem(params_T, stationary_sol_T, settings, E, Ω, T, p)
@@ -21,8 +21,8 @@ function solve_dynamics(params_T, stationary_sol_T, settings, T, Ω)
         # compute residual at t
         residual = zeros(length(u[1]))
         dae.dae_prob.f(residual, du[i], u[i], p, t)
-        residuals[i,:] = residual 
-        # compute stationary equilibrium at t 
+        residuals[i,:] = residual
+        # compute stationary equilibrium at t
         v_1 = u[i][1]
         g = u[i][M+1]
         z_hat = u[i][M+2]
@@ -49,26 +49,26 @@ function solve_dynamics(params_T, stationary_sol_T, settings, T, Ω)
     E = map(eq -> eq.E, equilibriums)
     entry_residual = map(eq -> eq.entry_residual, equilibriums)
 
-    return (v = v, g = g, z_hat = z_hat, 
+    return (v = v, g = g, z_hat = z_hat,
             S = S, L_tilde = L_tilde, z_bar = z_bar, π_min = π_min, π_tilde = π_tilde,
-            λ_ii = λ_ii, c = c, E = E, entry_residual = entry_residual, 
+            λ_ii = λ_ii, c = c, E = E, entry_residual = entry_residual,
             t = t, p = p, sol = sol, f! = dae.dae_prob.f, residuals = residuals, equilibriums = equilibriums)
 end
 
 # Implementation of the full model with time-varying objects, represented by DAE
 function PTW_DAEProblem(params_T, stationary_sol_T, settings, E, Ω, T, p)
-    # Unpack params and settings. 
-    @unpack z = settings 
+    # Unpack params and settings.
+    @unpack z = settings
     M = length(z)
-    @unpack L_1, L_2, z, M, T, μ, υ, σ, d, κ, ω, θ, δ, χ, N, ζ, ρ = p 
+    @unpack L_1, L_2, z, M, T, μ, υ, σ, d, κ, ω, θ, δ, χ, N, ζ, ρ = p
 
     get_S(g) = θ * (g - μ - θ * υ^2/2)
-    get_L_tilde(S, z_hat, E, Ω) = Ω * ((N-1) * z_hat^(-θ)*κ + ζ*(S + E / χ)) 
+    get_L_tilde(S, z_hat, E, Ω) = Ω * ((N-1) * z_hat^(-θ)*κ + ζ*(S + E / χ))
 
     function get_L_tilde_derivative_term(values_future, L_tilde_t, g_t, t)
         L_tilde_derivative_term = (params_T.γ - 1.0) * g_t # default value
         forward_index = findlast(x -> x[1] > t, values_future)
-        if ((T - t) > 1e-3 && forward_index != nothing;) # use callbacks only if t is well-separated from T 
+        if ((T - t) > 1e-3 && forward_index != nothing;) # use callbacks only if t is well-separated from T
             if (forward_index > 0)
             t_forward = values_future[forward_index][1]
             L_tilde_t_forward = values_future[forward_index][2]
@@ -80,18 +80,18 @@ function PTW_DAEProblem(params_T, stationary_sol_T, settings, E, Ω, T, p)
 
     function stationary_equilibrium(v_1, g, z_hat, E, Ω, t)
         S = get_S(g)
-        L_tilde = get_L_tilde(S, z_hat, E, Ω) 
+        L_tilde = get_L_tilde(S, z_hat, E, Ω)
         z_bar = Ω * (θ / (1 + θ - σ)) * (1 + (N-1) * d^(1-σ) * z_hat^(σ-1-θ))
         π_min = (1 - L_tilde) / ((σ-1)*z_bar)
         π_tilde = π_min * (1.0.+(N-1)*d^(1-σ)*(z .>= log(z_hat))) - (N-1)*κ*exp.(-(σ-1).*z).*(z .>= log(z_hat))
         entry_residual = v_1 - ζ * (1-χ) / χ
-        return (S = S, L_tilde = L_tilde, z_bar = z_bar, 
+        return (S = S, L_tilde = L_tilde, z_bar = z_bar,
                 π_min = π_min, π_tilde = π_tilde,
                 entry_residual = entry_residual)
     end
 
-    callback = SavingCallback((u,t,integrator)->(t, get_L_tilde(get_S(u[M+1]), u[M+2], E(t), Ω(t))), 
-                                p.saved_values, 
+    callback = SavingCallback((u,t,integrator)->(t, get_L_tilde(get_S(u[M+1]), u[M+2], E(t), Ω(t))),
+                                p.saved_values,
                                 tdir = -1) # need to compute D_t L(t)
 
     # Dynamic calculations, defined for each time ∈ t.
@@ -102,11 +102,11 @@ function PTW_DAEProblem(params_T, stationary_sol_T, settings, E, Ω, T, p)
         v = u[1:M]
         g = u[M+1]
         z_hat = u[M+2]
-        
+
         x = ζ
         Ω_t = Ω(t)
         E_t = E(t)
-        
+
         S = get_S(g)
         L_tilde = get_L_tilde(S, z_hat, E_t, Ω_t)
         z_bar = Ω(t) * (θ / (1.0 + θ - σ)) * (1.0 + (N-1) * d^(1-σ) * z_hat^(σ-1-θ))
@@ -118,24 +118,24 @@ function PTW_DAEProblem(params_T, stationary_sol_T, settings, E, Ω, T, p)
         L_tilde_derivative_term = get_L_tilde_derivative_term(values_future, L_tilde, g, t)
 
         # form the DAE at t
-        A_t = (ρ + δ + L_tilde_derivative_term - (σ - 1) * (μ - g + (σ - 1) * υ^2 / 2))*I - (μ - g + (σ-1)*υ^2)*L_1 - υ^2/2 * L_2        
+        A_t = (ρ + δ + L_tilde_derivative_term - (σ - 1) * (μ - g + (σ - 1) * υ^2 / 2))*I - (μ - g + (σ-1)*υ^2)*L_1 - υ^2/2 * L_2
         residual[1:M] = A_t * v - π_tilde # system of ODEs (eq:28)
         residual[M+1] = v[1] + x - dot(ω, v) # residual (eq:25)
-        residual[M+2] = z_hat^(σ-1) - κ * d^(σ-1) / π_min # export threshold (eq:31) 
-        residual[1:M] .-= du[1:M]    
+        residual[M+2] = z_hat^(σ-1) - κ * d^(σ-1) / π_min # export threshold (eq:31)
+        residual[1:M] .-= du[1:M]
     end
 
     u0 = [p.v_T; p.g_T; p.z_hat_T]
     du0 = zeros(M+2)
 
     return (dae_prob = DAEProblem(f!, du0, u0, (T, 0.0), differential_vars = [trues(M); false; false], p),
-            callback = callback, stationary_equilibrium = stationary_equilibrium)
+            callback = callback, stationary_equilibrium = stationary_equilibrium, f! = f!)
 end
 
 # return the parameters and functions needed to define dynamics
 function get_p(params_T, stationary_sol_T, settings, Ω, T)
     @unpack ρ, σ, N, θ, γ, d, κ, ζ, η, Theta, χ, υ, μ, δ = params_T
-    @unpack z = settings 
+    @unpack z = settings
     M = length(z)
 
     # Unpack the stationary solution.
@@ -147,11 +147,11 @@ function get_p(params_T, stationary_sol_T, settings, Ω, T)
     # Quadrature weighting
     ω = ω_weights(z, θ, σ-1)  # TODO: compute integral for eq 30
 
-    # Discretize the operator. 
-    z, L_1_minus, L_1_plus, L_2 = rescaled_diffusionoperators(z, σ-1) # L_1_minus ≡ L_1 is the only one we use. 
+    # Discretize the operator.
+    z, L_1_minus, L_1_plus, L_2 = rescaled_diffusionoperators(z, σ-1) # L_1_minus ≡ L_1 is the only one we use.
 
     # Bundle as before.
-    p = (L_1 = L_1_minus, L_2 = L_2, z = z, N = N, M = M, T = T, θ = θ, σ = σ, κ = κ, 
+    p = (L_1 = L_1_minus, L_2 = L_2, z = z, N = N, M = M, T = T, θ = θ, σ = σ, κ = κ,
         ζ = ζ, d = d, ρ = ρ, δ = δ, μ = μ, υ = υ, χ = χ, ω = ω, Ω = Ω,
         v_T = v_T, g_T = g_T, z_hat_T = z_hat_T, Ω_T = Ω_T,
         saved_values = SavedValues(Float64, Tuple{Float64,Float64})) #Named tuple for parameters.
