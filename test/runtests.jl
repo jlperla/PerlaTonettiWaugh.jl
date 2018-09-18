9a675678f49125023e88236a5acb6add75bf2bc3
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-09-18 10:59:44 -0700

    recover `runtests.jl`

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using PerlaTonettiWaugh, Test, LinearAlgebra
 using Distributions, Sundials, BenchmarkTools, Parameters, QuantEcon, Interpolations, NLsolve, Optim, DifferentialEquations, DiffEqCallbacks, Random
 
 @elapsed begin 
-#   @time @testset "Simple Stationary" begin include("simple/stationarytest.jl") end
-#   @time @testset "Full Stationary" begin include("full/stationarytest.jl") end
-#   @time @testset "Discretization and Rescaling Tests" begin include("discretizationtest.jl") end 
-#   @time @testset "Residuals/Dynamic ODE Tests" begin include("simple/residualstest.jl") end
+  @time @testset "Simple Stationary" begin include("simple/stationarytest.jl") end
+  @time @testset "Full Stationary" begin include("full/stationarytest.jl") end
+  @time @testset "Discretization and Rescaling Tests" begin include("discretizationtest.jl") end 
+  @time @testset "Residuals/Dynamic ODE Tests" begin include("simple/residualstest.jl") end
   @time @testset "Full Dynamic" begin include("full/dynamictest.jl") end
 end 
