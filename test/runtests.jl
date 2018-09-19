3e284567ff41dcf30be4ee640b64c4b0ebb9abf1
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-09-19 13:45:12 -0700

    restore `dynamictest.jl` in `runtests.jl`

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,5 +7,5 @@ using Distributions, Sundials, BenchmarkTools, Parameters, QuantEcon, Interpolat
   @time @testset "Full Stationary" begin include("full/stationarytest.jl") end
   @time @testset "Discretization and Rescaling Tests" begin include("discretizationtest.jl") end 
   @time @testset "Residuals/Dynamic ODE Tests" begin include("simple/residualstest.jl") end
-  # @time @testset "Full Dynamic" begin include("full/dynamictest.jl") end
+  @time @testset "Full Dynamic" begin include("full/dynamictest.jl") end
 end 
