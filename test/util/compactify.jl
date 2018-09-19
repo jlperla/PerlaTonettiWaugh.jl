363c2a5803376b02bd2937aeac754cfef5627681
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-09-19 13:37:06 -0700

    include more extreme values for try in unit tests for `compactify.jl`

--- test/util/compactify.jl
+++ test/util/compactify.jl
@@ -10,6 +10,8 @@ value_before_compactified_2 = ub + 19.2     # out of bounds
 value_before_compactified_3 = lb            # lb
 value_before_compactified_4 = ub            # ub
 value_before_compactified_5 = (lb + ub) / 2 # within bounds
+value_before_compactified_6 = lb - 124.1     # out of bounds, more extreme values
+value_before_compactified_7 = ub + 195.2     # out of bounds, more extreme values
 
 @test compactifier(-999999999.0) ≈ lb
 @test compactifier(999999999.0) ≈ ub
@@ -22,3 +24,5 @@ value_before_compactified_5 = (lb + ub) / 2 # within bounds
 @test decompactify_approximately(compactifier, compactifier(value_before_compactified_3)) ≈ value_before_compactified_3
 @test decompactify_approximately(compactifier, compactifier(value_before_compactified_4)) ≈ value_before_compactified_4
 @test decompactify_approximately(compactifier, compactifier(value_before_compactified_5)) ≈ value_before_compactified_5
+@test decompactify_approximately(compactifier, compactifier(value_before_compactified_6)) ≈ value_before_compactified_6
+@test decompactify_approximately(compactifier, compactifier(value_before_compactified_7)) ≈ value_before_compactified_7
