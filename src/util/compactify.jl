f50c0101f775b9448edafe3bd5d4f28703952b55
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-09-24 10:23:40 -0700

    add logistic types in `compactify.jl`

--- src/util/compactify.jl
+++ src/util/compactify.jl
@@ -1,15 +1,29 @@
-struct Compactifier
+abstract type TransformerType end
+struct UnitCircle <: TransformerType end
+struct Logistic <: TransformerType end
+
+# define logit/logistic functions
+logistic(x::Float64) = 1.0 / (1.0 + exp(-x))
+logit(x::Float64) = log(x / (1.0 - x))
+
+# define Compactifier
+struct Compactifier{T<:TransformerType}
     lb::Float64
     ub::Float64
     k::Float64 # half of the length between lb and ub
     a::Float64 # lb + k
-    Compactifier(lb::Float64, ub::Float64) = lb >= ub ? error("ub should be strictly greater than lb.") : new(lb, ub, (ub - lb)/2, (ub + lb)/2)
+    Compactifier(lb::Float64, ub::Float64) = Compactifier(lb::Float64, ub::Float64, UnitCircle())
+    Compactifier(lb::Float64, ub::Float64, transformer_type::TransformerType) = lb >= ub ? error("ub should be strictly greater than lb.") : new{typeof(transformer_type)}(lb, ub, (ub - lb)/2, (ub + lb)/2)
 end
-(f::Compactifier)(x) = f.a + f.k * x / sqrt(1 + x*x)
+(f::Compactifier{T})(x) where T <: UnitCircle = f.a + f.k * x / sqrt(1 + x*x)
+(f::Compactifier{T})(x) where T <: Logistic = f.lb + (f.ub - f.lb) * logistic(x) 
 
+# define Decompactifier
 struct Decompactifier
     compactifier::Compactifier
     Decompactifier(compactifier::Compactifier) = new(compactifier)
 end
-decompactify_approximately(f::Compactifier, y::Float64) = (y - f.a) / sqrt(1 - ((y-f.a)/f.k)^2) # decompactify with some numerical errors
+
+decompactify_approximately(f::Compactifier{T}, y::Float64) where T <: UnitCircle = (y - f.a) / sqrt(1 - ((y-f.a)/f.k)^2) 
+decompactify_approximately(f::Compactifier{T}, y::Float64) where T <: Logistic = logit((y - f.lb) / (f.ub - f.lb)) 
 (f::Decompactifier)(y) = decompactify_approximately(f.compactifier, y)
--- test/util/compactify.jl
+++ test/util/compactify.jl
@@ -1,4 +1,4 @@
-# setup
+# setup, unit circles
 lb = 0.0
 ub = 2.0
 compactifier = Compactifier(lb, ub)
@@ -12,6 +12,33 @@ value_before_compactified_4 = ub            # ub
 value_before_compactified_5 = (lb + ub) / 2 # within bounds
 value_before_compactified_6 = lb - 524.1     # out of bounds, more extreme values
 value_before_compactified_7 = ub + 595.2     # out of bounds, more extreme values
+
+@test compactifier(-999999999.0) ≈ lb
+@test compactifier(999999999.0) ≈ ub
+@test decompactifier(lb) ≈ -Inf
+@test decompactifier(ub) ≈ Inf
+@test compactifier(0.0) ≈ (lb + ub) / 2
+
+@test decompactifier(compactifier(value_before_compactified_1)) ≈ value_before_compactified_1
+@test decompactifier(compactifier(value_before_compactified_2)) ≈ value_before_compactified_2
+@test decompactifier(compactifier(value_before_compactified_3)) ≈ value_before_compactified_3
+@test decompactifier(compactifier(value_before_compactified_4)) ≈ value_before_compactified_4
+@test decompactifier(compactifier(value_before_compactified_5)) ≈ value_before_compactified_5
+@test decompactifier(compactifier(value_before_compactified_6)) ≈ value_before_compactified_6
+@test decompactifier(compactifier(value_before_compactified_7)) ≈ value_before_compactified_7
+
+# setup, logistic
+compactifier = Compactifier(lb, ub, PerlaTonettiWaugh.Logistic())
+decompactifier = Decompactifier(compactifier)
+
+# values for try
+value_before_compactified_1 = lb - 12.1     # out of bounds
+value_before_compactified_2 = ub + 19.2     # out of bounds 
+value_before_compactified_3 = lb            # lb
+value_before_compactified_4 = ub            # ub
+value_before_compactified_5 = (lb + ub) / 2 # within bounds
+value_before_compactified_6 = lb - 4.1     # out of bounds, less extreme values
+value_before_compactified_7 = ub + 1.2     # out of bounds, less extreme values
 
 @test compactifier(-999999999.0) ≈ lb
 @test compactifier(999999999.0) ≈ ub
