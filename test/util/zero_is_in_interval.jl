4e03d613adb411d1e51bdf7771e19cf692a576e8
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-09-26 13:34:44 -0700

    add more unit tests for `zero_is_in_interval`

--- test/util/zero_is_in_interval.jl
+++ test/util/zero_is_in_interval.jl
@@ -12,3 +12,21 @@ g = x -> x^2 - 1 # solution is x = 1 and -1
 @test zero_is_in_interval(g, -1.5, 1.5)
 @test zero_is_in_interval(g, -1.5, 0.0)
 @test zero_is_in_interval(g, 0.0, 1.5)
+
+g = x -> (x-1)^2 - 1 # solution is x = 2 and 0
+@test !zero_is_in_interval(g, 0.8, 1.2)
+@test !zero_is_in_interval(g, 0.5, 1.5)
+@test !zero_is_in_interval(g, -1.0, -0.5)
+@test !zero_is_in_interval(g, 2.5, 3.0)
+@test zero_is_in_interval(g, -2.5, 2.5)
+@test zero_is_in_interval(g, -2.5, 1.0)
+@test zero_is_in_interval(g, 1.0, 2.5)
+
+g = x -> (x+1)^2 - 1 # solution is x = 1 and -1
+@test !zero_is_in_interval(g, -1.2, -0.8)
+@test !zero_is_in_interval(g, -1.5, -0.5)
+@test !zero_is_in_interval(g, -3.0, -2.5)
+@test !zero_is_in_interval(g, 0.5, 1.0)
+@test zero_is_in_interval(g, -0.5, 0.5)
+@test zero_is_in_interval(g, -2.5, -1.0)
+@test zero_is_in_interval(g, -1.0, 0.5)
