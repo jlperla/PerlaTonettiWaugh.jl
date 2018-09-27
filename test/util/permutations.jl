5ce8974060aa7f706388992fa715a76449502e2e
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-09-27 11:02:11 -0700

    add an extra unit test for `get_all_permutations` using ranges

--- test/util/permutations.jl
+++ test/util/permutations.jl
@@ -49,3 +49,18 @@ ps = get_all_permutations([[1.0; 2.0], [3.0; 4.0], [5.0; 7.0; 9.0]], 3)
 @test [2.0; 3.0; 9.0] ∈ ps
 @test [2.0; 4.0; 9.0] ∈ ps
 @test length(ps) == 12
+
+ps = get_all_permutations([1.0:2.0, 3.0:4.0, 5.0:2.0:9.0], 3)
+@test [1.0; 3.0; 5.0] ∈ ps
+@test [1.0; 4.0; 5.0] ∈ ps
+@test [2.0; 3.0; 5.0] ∈ ps
+@test [2.0; 4.0; 5.0] ∈ ps
+@test [1.0; 3.0; 7.0] ∈ ps
+@test [1.0; 4.0; 7.0] ∈ ps
+@test [2.0; 3.0; 7.0] ∈ ps
+@test [2.0; 4.0; 7.0] ∈ ps
+@test [1.0; 3.0; 9.0] ∈ ps
+@test [1.0; 4.0; 9.0] ∈ ps
+@test [2.0; 3.0; 9.0] ∈ ps
+@test [2.0; 4.0; 9.0] ∈ ps
+@test length(ps) == 12
