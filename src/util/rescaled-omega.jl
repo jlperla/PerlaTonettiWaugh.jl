a02f0756635e7419cd7c03b035c26bb38a8efafa
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-10-01 15:46:03 -0700

    clarify that `E_hat` field of `RescaledΩ` is a function.

--- src/util/rescaled-omega.jl
+++ src/util/rescaled-omega.jl
@@ -2,7 +2,7 @@
 # Ω(0) = Ω_0 AND Ω(T) ≈ Ω_T AND E(T) ≈ δ
 struct RescaledΩ
     E::Function
-    E_hat
+    E_hat::Function
     Ω::Function
 
     function RescaledΩ(E_hat::Function, T::Float64, Ω_0::Float64, Ω_T::Float64, δ::Float64)
@@ -21,10 +21,10 @@ struct RescaledΩ
         E_hat_vec_range = E_hat_vec[end] - E_hat_vec[1]
         E_hat_vec_scaled = (E_hat_vec .- E_hat_vec[1]) ./ E_hat_vec_range .- 1.0 
         ts = range(0.0, stop=T, length=length(E_hat_vec))
-        E_hat = LinearInterpolation(ts, E_hat_vec_scaled) # might worth trying cubic spline
+        E_hat_interpolation = LinearInterpolation(ts, E_hat_vec_scaled) # might worth trying cubic spline
 
+        E_hat(t) = E_hat_interpolation(t)
         M = log(Ω_T/Ω_0) / quadgk(E_hat, 0, T)[1]
-
         E(t) = M*E_hat(t) + δ
         Ω(t) = Ω_0*exp(M*quadgk(E_hat, 0, t)[1]) 
 
