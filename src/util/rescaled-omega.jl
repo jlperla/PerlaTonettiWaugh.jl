d953b064ef00c14afd1bd38bb039f06bbfe0a7f1
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-10-05 13:00:23 -0700

    added documents outlining computation of `E`, `E_hat`, `Omega`
    Resolves #124.

--- src/util/rescaled-omega.jl
+++ src/util/rescaled-omega.jl
@@ -28,11 +28,11 @@ struct RescaledΩ
         E_hat_interpolation = Spline1D(ts, E_hat_vec_scaled; k = 3) # cubic spline
         E_hat(t) = E_hat_interpolation(t)
 
-        M = log(Ω_T/Ω_0) / integrate(E_hat_interpolation, 0, T)
-        Ω_derivative(Ω,p,t) = M*E_hat(t)*Ω
-        Ω_solution = DifferentialEquations.solve(ODEProblem(Ω_derivative,Ω_0,(0.0, T)), reltol = 1e-15)
+        M = log(Ω_T/Ω_0) / integrate(E_hat_interpolation, 0, T) # (eq:D.2)
+        Ω_derivative(Ω,p,t) = M*E_hat(t)*Ω # (eq:16), by replacing E(t) - δ by M*E_hat(t)*Ω from (eq:D.1)
+        Ω_solution = DifferentialEquations.solve(ODEProblem(Ω_derivative,Ω_0,(0.0, T)), reltol = 1e-15) # solution for (eq:16)
         Ω(t::Float64)::Float64 = t <= T ? Ω_solution(t) : Ω_solution(T)
-        E(t::Float64)::Float64 = M*E_hat(t) + δ
+        E(t::Float64)::Float64 = M*E_hat(t) + δ # (eq:D.1)
 
         new(E, E_hat, Ω)
     end
