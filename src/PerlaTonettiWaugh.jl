49b257a916e41685b16c2d2a76d7398338211f98
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-11-27 15:38:14 -0800

    Remove utility functions that are not being used anymore

--- src/PerlaTonettiWaugh.jl
+++ src/PerlaTonettiWaugh.jl
@@ -1,11 +1,9 @@
 module PerlaTonettiWaugh
 
 # Dependencies.
-using NLsolve, DifferentialEquations, BandedMatrices, Sundials, Distributions, Roots, QuantEcon, LinearAlgebra, Random, DataFrames, DataFramesMeta, DiffEqCallbacks, Interpolations, ContinuousTransformations, QuadGK, Dierckx, NLopt, ForwardDiff
+using NLsolve, DifferentialEquations, BandedMatrices, Sundials, Distributions, Roots, QuantEcon, LinearAlgebra, Random, DataFrames, DataFramesMeta, DiffEqCallbacks, Interpolations, QuadGK, Dierckx, NLopt, ForwardDiff
 import Parameters: @with_kw, @unpack
 
-# Utilities
-include("util/compactify.jl")
 # General discretization files.
 include("diffusionoperators.jl")
 include("quadrature.jl")
@@ -18,9 +16,5 @@ include("full/stationary.jl") # Includes params.
 include("full/dynamic.jl")
 
 export stationary_algebraic, stationary_numerical, simpleODE, simpleDAE, @with_kw, @unpack, stationary_algebraic_simple, stationary_numerical_simple, ω_weights, calculate_residuals, rescaled_diffusionoperators, diffusionoperators, solve_dynamics, entry_residuals
-
-
-# export utility for unit tests
-export Compactifier, Decompactifier
 
 end # module
