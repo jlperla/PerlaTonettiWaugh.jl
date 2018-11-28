c716901e4c521d50eadedf240c0e114f55bd8c62
Arnav Sood <soodarnav01@gmail.com>
2018-11-28 12:56:24 -0800

    refactor full stationary tests

--- test/full/stationarytest.jl
+++ test/full/stationarytest.jl
@@ -1,57 +1,100 @@
-# Define common objects. 
-    baselineparams = @with_kw (ρ = 0.02, σ = 4.2508, N = 10, θ = 5.1269, γ = 1.01, d = 2.3701, κ = 0.013, ζ = 1, η = 0, Theta = 1, χ = 1/(2.1868), υ = 0.0593, μ = 0, δ = 0.053) # Baselines per Jesse. 
+# User settings. 
+    # State grids. 
+    grids = [   range(0.0, 5.0, length = 500),
+                range(0.0, 7.0, length = 700),
+                unique([range(0.0, 5.0, length = 500)' range(5.0, 7.0, length = 200)']), # irregular 
+                unique([range(0.0, 5.0, length = 500)' range(0.0, 5.0, length = 330)']),
+            ]
+    # Overall parameters. 
+    params = @with_kw (ρ = 0.02, σ = 4.2508, N = 10, θ = 5.1269, γ = 1.01, d = 2.3701, κ = 0.013, ζ = 1,                    η = 0, Theta = 1, χ = 1/(2.1868), υ = 0.0593, μ = 0, δ = 0.053) 
+    baseline = params()
+    # Solver settings. 
+    initial_values = [  [0.25, 3.0, 1.0],
+                        [0.0190, 1.434969, 1.06517] # ~ equilibrium value for these parameters.
+                     ]
 
-#=
-    Algebraic tests. 
-=#
+# Execute tests. 
+@testset "Tests with Vanilla Parameters" begin 
+    # Compute algebraic solutions. 
+    algebraic_sols = stationary_algebraic.(Ref(baseline), initial_values)
+    # Test algebraic equilibrium quantities. 
+    # Growth rate tests. 
+    algebraic_gs = (x -> x.g).(algebraic_sols)
+    @test var(algebraic_gs) < 1e-10 # Tests that the solutions are similar to one another. 
+    @test all(algebraic_gs .≈ 0.01900455065415125) # Tests proximity to true value. 
+    # Ω tests. 
+    algebraic_Ωs = (x -> x.Ω).(algebraic_sols)
+    @test var(algebraic_Ωs) < 1e-10
+    @test all(algebraic_Ωs .≈ 1.06517755)
+    # z_hat tests. 
+    algebraic_zs = (x -> x.z_hat).(algebraic_sols)
+    @test var(algebraic_zs) < 1e-10
+    @test all(algebraic_zs .≈ 1.434969541725385)
 
-# Run for some different starting poitns (arbitrarily chosen).
-    init_x1 = [3.0, -1, -2] # For numerical
-    init_x2 = [0.25, 3.0, 1.0] # Breaks around 0.3 for g. 
-    init_x3 = [0.0190, 1.434969, 1.06517] # ~ equilibrium value. 
+    # Compute numerical solutions.
+    numerical_sols = stationary_numerical.(Ref(baseline), grids)
+    # Test numerical equilibrium quantities. 
+    numerical_gs = (x -> x.g).(numerical_sols)
+    @test_broken var(numerical_gs) < 1e-10 # Tests that the solutions are similar to one another. 
+    @test_broken all(numerical_gs .≈ 0.01900455065415125) # Tests proximity to true value. 
+    # Ω tests. 
+    numerical_Ωs = (x -> x.Ω).(numerical_sols)
+    @test_broken var(numerical_Ωs) < 1e-10
+    @test_broken all(numerical_Ωs .≈ 1.06517755)
+    # z_hat tests. 
+    numerical_zs = (x -> x.z_hat).(numerical_sols)
+    @test_broken var(numerical_zs) < 1e-10
+    @test_broken all(numerical_zs .≈ 1.434969541725385)
 
-    # Tests for vanilla parameters. 
-    # Broken res1_alg = stationary_algebraic(baselineparams(), init_x1)
-    res2_alg = stationary_algebraic(baselineparams(), init_x2)
-    res3_alg = stationary_algebraic(baselineparams(), init_x3)
-    @test res2_alg.g ≈ res3_alg.g atol = atol = 1e-5 
+    # Numerical residuals tests. 
+    for i in 1:length(numerical_sols)
+        # Get values to test. 
+        grid = grids[i]
+        sol = numerical_sols[i]
+        # Compute interim quantities. 
+        ω = ω_weights(grid, baseline.θ, baseline.σ-1)
+        value_matching = sol.v_tilde[1] - dot(sol.v_tilde, ω) + sol.x
+        free_entry = sol.v_tilde[1] - sol.x*(1-baseline.χ)/baseline.χ
+        # Test. 
+        @test value_matching < 1e-8
+        @test_broken free_entry < 1e-8
+    end     
+end 
 
-    # Tests for new d. BROKEN FOR NOW. 
-    # res1_new = stationary_algebraic(baselineparams(d = 5), init_x1)
-    # res2_new = stationary_algebraic(baselineparams(d = 5), init_x2) 
-    # # BROKEN res3_new = stationary_algebraic(baselineparams(d = 5), init_x3)
-    # @test res1_new.g ≈ res3_new.g atol = 1e-5
-    # @test res1_new.g ≈ res2_new.g atol = 1e-5
+@testset "Tests with Perturbed Parameters" begin 
+    # Generate new parameters. 
+    newparams = params(σ = 4.0)
+    # Compute algebraic solutions. 
+    algebraic_sols = stationary_algebraic.(Ref(newparams), initial_values)
+    # Test consistency. 
+    algebraic_gs = (x -> x.g).(algebraic_sols)
+    algebraic_Ωs = (x -> x.Ω).(algebraic_sols)
+    algebraic_zs = (x -> x.z_hat).(algebraic_sols)
+    @test var(algebraic_gs) < 1e-10 # Tests that the solutions are similar to one another. 
+    @test var(algebraic_Ωs) < 1e-10
+    @test var(algebraic_zs) < 1e-10
+    
+    # Compute numerical solutions. 
+    numerical_sols = stationary_numerical.(Ref(newparams), grids)
+    # Test consistency. 
+    numerical_gs = (x -> x.g).(numerical_sols)
+    numerical_Ωs = (x -> x.Ω).(numerical_sols)
+    numerical_zs = (x -> x.z_hat).(numerical_sols)
+    @test_broken var(numerical_gs) < 1e-10 # Tests that the solutions are similar to one another. 
+    @test_broken var(numerical_Ωs) < 1e-10
+    @test_broken var(numerical_zs) < 1e-10
 
-    # Test for error handling. 
-    # @test_throws ErrorException stationary_algebraic(baselineparams(ζ = 0.0001, γ = 0.24))
-
-# Benchmarks.
-    # @btime result = stationary_algebraic(x1, baselineparams())
-    # @btime result = stationary_algebraic([0.15, 4.0, 2.6], baselineparams()) # Try different params. 
-    # Anderson acceleration? Need to find a paramset that works. 
-
-#= 
-    Numerical tests. 
-=#
-    z = unique([range(0.0, stop = 1.0, length = 1000)' range(1.0, stop = 2.0, length = 250)' range(2.0, stop = 7.0, length = 100)'])
-    res1_num = stationary_numerical(baselineparams(), z, init_x1) 
-    res2_num = stationary_numerical(baselineparams(), z, init_x2)
-    res3_num = stationary_numerical(baselineparams(), z, init_x3)
-    res_def_num = stationary_numerical(baselineparams(), z)
-
-    # Residuals
-    x = res1_num.x 
-    ω = ω_weights(z, baselineparams().θ, baselineparams().σ-1)
-    value_matching = v_tilde -> v_tilde[1] - dot(v_tilde, ω) + x
-    free_entry = v_tilde -> v_tilde[1] - x*(1-baselineparams().χ)/baselineparams().χ
-    @test value_matching(res1_num.v_tilde) ≈ 0.0 atol = 1e-8
-    @test free_entry(res1_num.v_tilde) ≈ 0.0 atol = 1e-8
-
-    # Similar to each other. 
-    @test res1_num.g ≈ res2_num.g 
-    @test res2_num.g ≈ res3_num.g 
-    @test res3_num.g ≈ res_def_num.g 
-
-    # Similar-ish to algebraic. 
-    @test res1_num.g ≈ res2_alg.g atol = 1e-1
+    # Numerical residuals tests. 
+    for i in 1:length(numerical_sols)
+        # Get values to test. 
+        grid = grids[i]
+        sol = numerical_sols[i]
+        # Compute interim quantities. 
+        ω = ω_weights(grid, baseline.θ, baseline.σ-1)
+        value_matching = sol.v_tilde[1] - dot(sol.v_tilde, ω) + sol.x
+        free_entry = sol.v_tilde[1] - sol.x*(1-baseline.χ)/baseline.χ
+        # Test. 
+        @test value_matching < 1e-8
+        @test_broken free_entry < 1e-8
+    end    
+end 
