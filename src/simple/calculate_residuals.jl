651cdee8298398cf58dcedfed33786667147a0f8
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-12-03 15:53:08 -0800

    Add `t_grid` in tstops when residuals are calculated in simple models

--- src/simple/calculate_residuals.jl
+++ src/simple/calculate_residuals.jl
@@ -11,7 +11,7 @@ end
 
 function calculate_residuals(ode_prob, x, ω, ode_solve_algorithm, ts) # To keep the params consistent with other tuples.
     # Solve ode
-    sol = Sundials.solve(ode_prob, ode_solve_algorithm)
+    sol = Sundials.solve(ode_prob, ode_solve_algorithm, tstops = ts)
     M = length(ω)
     # Calculate the residual at each time point
     residuals = zeros(length(ts))
