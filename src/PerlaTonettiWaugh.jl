2b851594cda05b99da8833a441797744d3649495
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-12-06 12:02:21 -0800

    Add solve-full-model.jl and corresponding notebook for example
    Note that the notebook does not use an optimized solution yet.

--- src/PerlaTonettiWaugh.jl
+++ src/PerlaTonettiWaugh.jl
@@ -3,6 +3,7 @@ module PerlaTonettiWaugh
 # Dependencies.
 using NLsolve, DifferentialEquations, BandedMatrices, Sundials, Distributions, Roots, QuantEcon, LinearAlgebra, Random, DataFrames, DataFramesMeta, DiffEqCallbacks, Interpolations, QuadGK, Dierckx, NLopt, ForwardDiff
 using LeastSquaresOptim
+using BlackBoxOptim
 import Parameters: @with_kw, @unpack
 import DFOLS
 
@@ -16,8 +17,10 @@ include("simple/calculate_residuals.jl")
 # Full model.
 include("full/stationary.jl") # Includes params.
 include("full/dynamic.jl")
+include("full/solve-full-model.jl")
 
 export stationary_algebraic, stationary_numerical, simpleODE, simpleDAE, @with_kw, @unpack, stationary_algebraic_simple, stationary_numerical_simple, ω_weights, calculate_residuals, rescaled_diffusionoperators, diffusionoperators, solve_dynamics, entry_residuals, welfare
 export minimize_residuals, minimize_residuals_python
+export solve_full_model_global
 
 end # module
