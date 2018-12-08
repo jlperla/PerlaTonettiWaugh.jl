0616d99f625cf2141af5c5f75e9ca2be6927812e
Arnav Sood <soodarnav01@gmail.com>
2018-12-07 16:43:28 -0800

    return DFOLS object too

--- src/full/solve-full-model.jl
+++ src/full/solve-full-model.jl
@@ -13,7 +13,7 @@ end
 function solve_full_model_python(settings; user_params = nothing)
   settings = merge(settings, (sort_candidate = false,))
   result = DFOLS.solve(x -> residuals_given_candidate(x, settings), settings.global_transition_x0, user_params = user_params)
-  return (solution = solve_with_candidate(result.x, settings; detailed_solution = true), E_nodes_and_T = result.x)
+  return (solution = solve_with_candidate(result.x, settings; detailed_solution = true), E_nodes_and_T = result.x, solobj = result)
 end
 
 function solve_with_candidate(candidate, settings; detailed_solution = false, interp = CubicSplineInterpolation)
