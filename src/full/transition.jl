4254c8a26223b3d34e037fb8e22e2ab33c4e94d6
Arnav Sood <soodarnav01@gmail.com>
2018-12-12 15:49:31 -0800

    use Linear Interpolation in the solve_given_E_nodes or whatever

--- src/full/transition.jl
+++ src/full/transition.jl
@@ -7,7 +7,7 @@
 # Continuation solver. Essentially traces out a smooth path from some d_T to d_0, using the result from one step as the x0 for the next.
 # With default parameters we get: [-1.1965566608914664,-0.7506441878495663,-0.6160629809004126,-0.4490604063250066,-0.3457782022324279,-0.26611899012195817,-0.1703597837124977,-0.12538756277279978,-0.10765936098467942,-0.057163395444530966,-0.05297636889833756,-0.032260206198113685,-0.028143386570548785,-0.04434566417370368]
 
-# 
+#
 function solve_continuation(d_0, d_T; step = 0.005, params = parameter_defaults(), settings = settings_defaults(), solver = solve_full_model_python)
   params_0 = merge(params, (d = d_T,)) # parameters to be used at t = 0
   params_T = merge(params, (d = d_T,)) # parameters to be used at t = T
@@ -24,7 +24,7 @@ function solve_continuation(d_0, d_T; step = 0.005, params = parameter_defaults(
     Ω_0 = stationary_numerical(params_0, z_grid).Ω
     settings = merge(settings, (Ω_0 = Ω_0,))
     result = solver(settings)
-    settings = merge(settings, (transition_x0 = result.E_nodes,)) # this is agnostic to the solver 
+    settings = merge(settings, (transition_x0 = result.E_nodes,)) # this is agnostic to the solver
   end
   return settings, result
 end
@@ -33,7 +33,7 @@ end
 function solve_full_model_global(settings; impose_E_monotonicity_constraints = true)
   settings = merge(settings, (iterations = settings.transition_iterations, weights = settings.transition_weights))
   ranges = map(i->(settings.transition_lb[i], settings.transition_ub[i]), 1:length(settings.transition_x0))
-  result = bboptimize(x -> ssr_given_E_nodes_interior(impose_E_monotonicity_constraints ? sort(x) : x, settings); 
+  result = bboptimize(x -> ssr_given_E_nodes_interior(impose_E_monotonicity_constraints ? sort(x) : x, settings);
                       SearchRange = ranges, NumDimensions = length(ranges), MaxSteps = settings.iterations)
   return (solution = solve_model_from_E_nodes(best_candidate(result), settings; detailed_solution = true), E_nodes = best_candidate(result))
 end
@@ -41,7 +41,7 @@ end
 
 function solve_full_model_newuoa(settings)
   result = solve_system(x -> residuals_given_E_nodes_interior(x, settings), settings.transition_x0;
-                    lb = nothing, ub = fill(0.0, length(settings.transition_x0)), 
+                    lb = nothing, ub = fill(0.0, length(settings.transition_x0)),
                     autodiff = :finite, algorithm = :LN_NEWUOA_BOUND)
   return (solution = solve_model_from_E_nodes(result, settings; detailed_solution = true),
           E_nodes = result)
@@ -78,12 +78,12 @@ end
 =#
 
 # Returns model solution (i.e., solve_dynamics output) given a set of E nodes. Used by all solvers.
-function solve_model_from_E_nodes(E_nodes_interior, settings; detailed_solution = false, interp = CubicSplineInterpolation)
+function solve_model_from_E_nodes(E_nodes_interior, settings; detailed_solution = false, interp = LinearInterpolation)
   @unpack T, params_T, stationary_sol_T, Ω_0, E_node_count, entry_residuals_nodes_count, iterations = settings
   δ = params_T.δ
   Ω_T = stationary_sol_T.Ω
   # fix the point at T to be zero and sort candidate if needed
-  E_nodes = [-1.0; E_nodes_interior; 0.0] # fix the end points to remove indeterminacy problems 
+  E_nodes = [-1.0; E_nodes_interior; 0.0] # fix the end points to remove indeterminacy problems
   ts = range(0.0, stop=T, length=length(E_nodes))
   E_hat_interpolation = interp(ts, E_nodes) # might worth trying cubic spline
   E_hat(t) = E_hat_interpolation(t)
