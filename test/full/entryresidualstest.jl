afbc8c3780bd12a188e5a5b27a7f2f279926e6a3
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2018-12-12 16:08:58 -0800

    Replace entry_residuals with residuals_given_E_nodes_interior
    For #176

--- test/full/entryresidualstest.jl
+++ test/full/entryresidualstest.jl
@@ -1,69 +1,55 @@
 @testset "entry_residuals tests" begin 
-    # User settings. 
-    # Space grid. 
-    z_min = 0.0 
-    z_max = 5.0 
-    M = 1000
-    # Experiment settings. 
-    d_0 = 5.0
-    d_T = 2.3701
-    Δ_E = 1e-06
-    # Overall parameters. 
-    params = (ρ = 0.02, σ = 4.2508, N = 10, θ = 5.1269, γ = 1.00, κ = 0.013, ζ = 1, η = 0, Theta = 1, χ = 1/(2.1868), υ = 0.0593, μ = 0, δ = 0.053)
+    @testset "entry_residuals with default Ω" begin
+        # Define common objects. 
+        params = parameter_defaults()
 
-    # Construct intermediate objects. 
-    z = range(z_min, stop = z_max, length = M)
-    params_0 = merge(params, (d = d_0,))
-    params_T = merge(params, (d = d_T,))
-
-    # Get (numerical) stationary solution. 
-    stationary_0 = stationary_numerical(params_0, z)
-    stationary_T = stationary_numerical(params_T, z)
-
-    # Process those 
-    Ω_0 = stationary_0.Ω
-    Ω_T = stationary_T.Ω
-    v_T = stationary_T.v_tilde
-    g_T = stationary_T.g 
-    z_hat_T = stationary_T.z_hat 
-
-    # Solver settings. 
-    tstops = tstops = range(0.0, stop = T, length = 100) 
-    settings = (z = z, tstops = tstops, Δ_E = Δ_E)
-
-    @testset "entry_residuals with constant Ω" begin
-        # Define more interim quantities. 
-        T = sqrt(2*(log(Ω_0) - log(Ω_T)) / params.δ) 
-        Ω(t) = Ω_T # Constant Ω
-        E = t -> (log(Ω(t + Δ_E)) - (log(t - Δ_E)))/(2*Δ_E) + params.δ # Central differences. 
-        
-        # Objects for interpolation. 
-        Ω_nodes = range(0.0, stop=T, length=30)
-        Ω_nodes_interior = Ω_nodes[2:(end-1)]
-        entry_residuals_nodes = Ω_nodes
-
-        Ω_interior = map(t -> Ω(t), Ω_nodes_interior)
-
-        # Tests; note that we forced Ω_0 to be Ω_T to make Ω constant over t = 0 too
-        residuals_interp = entry_residuals(Ω_interior, (Ω_T), stationary_T, T, params_T, settings, Ω_nodes, entry_residuals_nodes).entry_residuals_interpolation
-        @test mean(residuals_interp.(entry_residuals_nodes)) ≈ 0.0 atol = 1 # since `Ω` is not in equilibrium, residuals can be high
+        settings = settings_defaults()
+        settings = merge(settings, (global_transition_penalty_coefficient = 1.0, ))
+        z_grid = settings.z
+        M = length(z_grid)
+    
+        d_0 = params.d_0
+        d_T = params.d_T
+        params_0 = merge(params, (d = d_T,)) # parameters to be used at t = 0
+        params_T = merge(params, (d = d_0,)) # parameters to be used at t = T
+    
+        # solve for stationary solution at t = 0
+        stationary_sol_0 = stationary_numerical(params_0, z_grid) # solution at t = 0
+        stationary_sol = stationary_numerical(params_T, z_grid) # solution at t = T
+    
+        Ω_0 = stationary_sol_0.Ω;
+        Ω_T = stationary_sol.Ω;
+        settings = merge(settings, (params_T = params_T, stationary_sol_T = stationary_sol, Ω_0 = Ω_0))
+    
+        E_nodes_interior = zeros(length(settings.transition_x0))
+        residuals = residuals_given_E_nodes_interior(E_nodes_interior, settings)
+        @test mean(residuals) ≈ 0.0 atol = 1e-3
     end
 
-    @testset "entry_residuals with exponential Ω" begin
-        # Define more interim quantities. 
-        T = sqrt(2*(log(Ω_0) - log(Ω_T)) / params.δ) 
-        Ω(t) = t < T ? Ω_0 * exp(-params.δ*T*t + params.δ*t*t/2) : Ω_T # Exponential Ω with time smoothing
-        E = t -> (log(Ω(t + Δ_E)) - (log(t - Δ_E)))/(2*Δ_E) + params.δ # Central differences. 
+    @testset "entry_residuals with constant Ω" begin
+        # Define common objects. 
+        params = parameter_defaults()
+
+        settings = settings_defaults()
+        settings = merge(settings, (global_transition_penalty_coefficient = 1.0, ))
+        z_grid = settings.z
+        M = length(z_grid)
     
-        # Objects for interpolation. 
-        Ω_nodes = range(0.0, stop=T, length=30)
-        Ω_nodes_interior = Ω_nodes[2:(end-1)]
-        entry_residuals_nodes = Ω_nodes
+        d_0 = params.d_0
+        d_T = params.d_T
+        params_0 = merge(params, (d = d_T,)) # parameters to be used at t = 0
+        params_T = merge(params, (d = d_T,)) # parameters to be used at t = T
     
-        Ω_interior = map(t -> Ω(t), Ω_nodes_interior)
+        # solve for stationary solution at t = 0
+        stationary_sol_0 = stationary_numerical(params_0, z_grid) # solution at t = 0
+        stationary_sol = stationary_numerical(params_T, z_grid) # solution at t = T
     
-        # Tests. 
-        residuals_interp = entry_residuals(Ω_interior, (Ω_0), stationary_T, T, params_T, settings, Ω_nodes, entry_residuals_nodes).entry_residuals_interpolation
-        @test mean(residuals_interp.(entry_residuals_nodes)) ≈ 0.0 atol = 1 # since `Ω` is not in equilibrium, residuals can be high
+        Ω_0 = stationary_sol_0.Ω;
+        Ω_T = stationary_sol.Ω;
+        settings = merge(settings, (params_T = params_T, stationary_sol_T = stationary_sol, Ω_0 = Ω_0))
+    
+        E_nodes_interior = settings.transition_x0        
+        residuals = residuals_given_E_nodes_interior(E_nodes_interior, settings)
+        @test mean(residuals) ≈ 0.0 atol = 1e-3
     end
 end 
