ed7e56baea259a508647005c14f251ce330c17c4
mwaugh <mwaugh@stern.nyu.edu>
2019-01-09 12:20:29 -0500

    mike updates

--- src/utils/consumption-equivalent.jl
+++ src/utils/consumption-equivalent.jl
@@ -1,1 +1,4 @@
 consumption_equivalent(U_1, U_2, params) = exp(params.ρ*(U_1 - U_2)) # (B.78) from computational appendix
+# Here U_1 is the NEW untility level
+# U_2 is the OLD utility level. 
+# Should change notation to make clearer at somepoint.
