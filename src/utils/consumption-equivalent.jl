f96bf9c4d6c7a4a8a353bdcebf39eeedc884fddb
Arnav Sood <soodarnav01@gmail.com>
2019-01-18 12:37:48 -0800

    clean up consumption-equivalent.jl

--- src/utils/consumption-equivalent.jl
+++ src/utils/consumption-equivalent.jl
@@ -1,4 +1,1 @@
-consumption_equivalent(U_1, U_2, params) = exp(params.ρ*(U_1 - U_2)) # (B.78) from computational appendix
-# Here U_1 is the NEW untility level
-# U_2 is the OLD utility level. 
-# Should change notation to make clearer at somepoint.
+consumption_equivalent(U, U_old, params) = exp(params.ρ*(U - U_old)) # (B.78) from computational appendix
