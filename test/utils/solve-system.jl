51bc8b1438fc8fe4c494b3e069bed6ed4a4f592a
Arnav Sood <soodarnav01@gmail.com>
2019-01-18 16:21:28 -0800

    tweak

--- test/utils/solve-system.jl
+++ test/utils/solve-system.jl
@@ -5,4 +5,5 @@
             h(x,a) = (x - a) # residual measures how much x is different from a
             x0 = [5.0; 5.0]
             @test solve_system(x -> h(x,a), x0; lb = fill(0.0, length(x0))) ≈ a
+        end 
 end
