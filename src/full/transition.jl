a9a7bfb64379e3d15cb92deacdccc4ea666eb2b0
Arnav Sood <soodarnav01@gmail.com>
2019-02-07 11:30:04 -0800

    mild cleanup of the transition file

--- src/full/transition.jl
+++ src/full/transition.jl
@@ -6,88 +6,58 @@
 
 # Global solver.
 function solve_full_model_global(settings; impose_E_monotonicity_constraints = true, front_nodes_appended = nothing)
-  if (settings.transition_iterations < 1)
-    E_nodes_interior = settings.transition_x0
-    return (solution = solve_model_from_E_nodes(E_nodes_interior, settings; detailed_solution = true), E_nodes = E_nodes_interior)
-  end
-
-  # exception handling
-  if (length(settings.transition_lb) != length(settings.transition_ub))
-    throw("length(settings.transition_lb) != length(settings.transition_ub)")
-  end
-  if (length(settings.transition_x0) != length(settings.transition_ub))
-    @warn "length(settings.transition_x0) != length(settings.transition_ub); 
-          setting transition_ub as vector of 0.0 with the same length as transition_x0"
-    settings = merge(settings, (transition_ub = fill(0.0, length(settings.transition_x0)),))
-  end
-  if (length(settings.transition_x0) != length(settings.transition_lb))
-    @warn "length(settings.transition_x0) != length(settings.transition_lb); 
-          setting transition_lb as vector of -1.0 with the same length as transition_x0"
-    settings = merge(settings, (transition_lb = fill(-1.0, length(settings.transition_x0)),))
-  end
-  if (length(settings.transition_x0) != length(settings.weights))
-    @warn "length(settings.transition_x0) != length(settings.weights); 
-          setting weights as (x -> x < 10 ? 10. - x : 1.).(1:1:length(settings.transition_x0))"
-    settings = merge(settings, (weights = (x -> x < 10 ? 10. - x : 1.).(1:1:length(settings.transition_x0)),))
-  end
-
+  # some exception handling on the inputs
+  length(settings.transition_lb) == length(settings.transition_ub) || @warn "length(settings.transition_lb) != length(settings.transition_ub)"
+  length(settings.transition_x0) == length(settings.transition_ub) || @warn "transition_x0 and transition_ub sizes differ; setting ub to zeros(size(transition_x0))"; settings = merge(settings, (transition_ub = fill(0.0, length(settings.transition_x0)),))
+  length(settings.transition_x0) == length(settings.transition_lb) || @warn "transition_x0 and transition_lb sizes differ; setting lb to -1*ones(size(transition_x0))"; settings = merge(settings, (transition_lb = fill(-1.0, length(settings.transition_x0)),))
+  length(settings.transition_x0) == length(settings.weights) || @warn "transition_x0 and weights sizes differ; setting weights to default function"; settings = merge(settings, (weights = (x -> x < 10 ? 10. - x : 1.).(1:1:length(settings.transition_x0)),))
   # setup range for bboptimize
   ranges = map(i->(settings.transition_lb[i], settings.transition_ub[i]), 1:length(settings.transition_ub))
-
-  ssr(residuals) = sum(residuals.^2) + # squared sum of residuals
-    settings.transition_penalty_coefficient * sum(max.(-diff(residuals), 0.0)) # add penalty for non-increasing sol
-  result = bboptimize(x -> ssr(weighted_residuals_given_E_nodes_interior(impose_E_monotonicity_constraints ? sort(x) : x, settings; front_nodes_appended = front_nodes_appended));
-                      SearchRange = ranges, NumDimensions = length(ranges), MaxSteps = settings.transition_iterations)
+  # run solver and process result
+  ssr(residuals) = sum(residuals.^2) + settings.transition_penalty_coefficient * sum(max.(-diff(residuals), 0.0)) # add penalty for non-increasing sol
+  result = bboptimize(x -> ssr(weighted_residuals_given_E_nodes_interior(impose_E_monotonicity_constraints ? sort(x) : x, settings; front_nodes_appended = front_nodes_appended)); SearchRange = ranges, NumDimensions = length(ranges), MaxSteps = settings.transition_iterations)
   E_nodes_found = best_candidate(result)
   E_nodes_found = impose_E_monotonicity_constraints ? sort(E_nodes_found) : E_nodes_found
-
+  # return
   return (solution = solve_model_from_E_nodes(E_nodes_found, settings; detailed_solution = true), E_nodes = E_nodes_found)
 end
 
-function solve_full_model(settings; impose_E_monotonicity_constraints = true, write_csv = false, csvpath = nothing,
-                        run_global = true, front_nodes_appended = nothing)
-
-  if (run_global && settings.transition_iterations > 0) 
-    result = solve_full_model_global(settings; 
-              impose_E_monotonicity_constraints = impose_E_monotonicity_constraints, 
-              front_nodes_appended = front_nodes_appended)
+function solve_full_model(settings; impose_E_monotonicity_constraints = true, write_csv = false, csvpath = nothing, run_global = true, front_nodes_appended = nothing)
+  # preprocessing before NLopt/local solver
+  if (run_global && settings.transition_iterations > 0) # run global if required
+    result = solve_full_model_global(settings; impose_E_monotonicity_constraints = impose_E_monotonicity_constraints, front_nodes_appended = front_nodes_appended)
     E_nodes = result.E_nodes;
     settings = merge(settings, (transition_x0 = E_nodes, ));
+  elseif transition_iterations < 1 # return immediately with the initial condition if that's required
+    E_nodes_interior = settings.transition_x0
+    return (solution = solve_model_from_E_nodes(E_nodes_interior, settings; detailed_solution = true), E_nodes = E_nodes_interior)
   end
-  
-   # constraint for increasing E nodes
+   # linear constraint for increasing E nodes
   function constraints_increasing_E!(h, x, jacobian_t)
     L = length(x)
     # A is a matrix whose ith row has 1 in ith col and -1 in (i+1)th col
     # so ith element of A*x imposes x[i] <= x[i+1]
     # note that this imposes x[end] <= 0 as well as the last row is [0; 0; ...; 0; 1].
     A = LinearAlgebra.Tridiagonal(zeros(L-1),ones(L),-ones(L-1))
-    if length(jacobian_t) > 0 # transpose of the Jacobian matrix
-        jacobian_t[:] = A'
+    if length(jacobian_t) > 0
+      jacobian_t[:] = A' # transpose of the Jacobian matrix
     end
     h[:] = A*x
   end
+  # run the local (NLopt) solver
   E_nodes = solve_system(x -> weighted_residuals_given_E_nodes_interior(x, settings; front_nodes_appended = front_nodes_appended), settings.transition_x0;
                     lb = nothing, ub = fill(0.0, length(settings.transition_x0)),
                     constraints_fg! = impose_E_monotonicity_constraints ? constraints_increasing_E! : nothing,
                     algorithm = impose_E_monotonicity_constraints ? :LD_SLSQP : :LD_LBFGS,
                     iterations = settings.transition_iterations)
-
-  # append front_nodes_appended in front if needed 
+  # append front_nodes_appended in front if needed
     E_nodes = front_nodes_appended == nothing ? E_nodes : [front_nodes_appended; E_nodes]
     solution = solve_model_from_E_nodes(E_nodes, settings; detailed_solution = true)
-
-    # output caching
-    if write_csv
-      CSV.write(csvpath, solution.results)
-    end
-
+  # output caching
+    write_csv && CSV.write(csvpath, solution.results)
+  # return
   return (solution = solution, E_nodes = E_nodes)
 end
-
-#=
-  Auxiliary functions. Organized from most fundamental to least fundamental.
-=#
 
 # Returns model solution (i.e., solve_dynamics output) given a set of E nodes. Used by all solvers.
 function solve_model_from_E_nodes(E_nodes_interior, settings; detailed_solution = false, interp = LinearInterpolation)
@@ -111,15 +81,13 @@ function solve_model_from_E_nodes(E_nodes_interior, settings; detailed_solution 
 end
 
 function weighted_residuals_given_E_nodes_interior(E_nodes_interior, settings; front_nodes_appended = nothing)
-  etnry_residuals_nodes_count = length(E_nodes_interior)
-
+  entry_residuals_nodes_count = length(E_nodes_interior)
   # append front_nodes_appended in front if needed
   E_nodes_interior = front_nodes_appended == nothing ? E_nodes_interior : [front_nodes_appended; E_nodes_interior]
   # solve the model using E_nodes_interior received
-  solved = try solve_model_from_E_nodes(E_nodes_interior, settings).results catch; return fill(10e20, etnry_residuals_nodes_count) end
+  solved = try solve_model_from_E_nodes(E_nodes_interior, settings).results catch; return fill(10e20, entry_residuals_nodes_count) end
   entry_residual_interpolated = LinearInterpolation(solved.t, solved.entry_residual)
-  entry_residuals_nodes = range(0, stop = solved.t[end], length = etnry_residuals_nodes_count + 2)
-
+  entry_residuals_nodes = range(0, stop = solved.t[end], length = entry_residuals_nodes_count + 2)
   # return weighted residuals
   return settings.weights .* entry_residual_interpolated.(entry_residuals_nodes[2:(end-1)])
 end
