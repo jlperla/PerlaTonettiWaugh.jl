7762beefd146d775acb8184829c8437da59e5319
Arnav Sood <soodarnav01@gmail.com>
2019-04-01 16:45:28 -0700

    edits to simple dynamic

--- src/simple/dynamic.jl
+++ src/simple/dynamic.jl
@@ -1,35 +1,45 @@
 # DAE kernel, defined for each time ∈ t.
 function f!_simple(resid,du,u,p,t)
-    @unpack L_1, L_2, z, r, μ, g, υ, π, T, P, x, ω = p
+    @unpack Ξ₁, L_1, L_2, z, r, μ, g, υ, π, T, P, x, ω = p
     # Carry out calculations.
     v_t = u[1:P]
     g_t = u[P+1]
     A = (r(t) - μ - υ^2/2)*I - (μ + υ^2 - g_t)*L_1 - υ^2/2 * L_2 # (17)
     # Calculate residuals.
-    resid[1:P] .= A * v_t - π.(t, z) # (22)
-    resid[1:P] .-= du[1:P] # discretized system of ODE for v (22)
-    resid[P+1] = v_t[1] + x(t) - dot(ω, v_t) # value matching condition (23)
+    resid[1:P] .= A * v_t - π.(t, z) # (26)
+    resid[1:P] .-= du[1:P] # discretized system of ODE for v (26)
+    resid[P+1] = Ξ₁*v_t[1] - dot(ω, v_t) + x(t) # value matching condition (27)
 end
 
 # DAE constructor
 function simpleDAE(params, settings)
-    # Unpack necessary objects.
+    # Unpack necessary objects
     @unpack μ, υ, θ, r, x, π = params
-    @unpack z, T, g = settings
+    @unpack z_ex, T, g = settings
+    
+    @assert z_ex[1] == 0.0 && issorted(z_ex) # validate grid 
+    z = z_ex[2:end-1]
     P = length(z)
+
     # Quadrature weighting
-    ω = ω_weights(z, θ, 1)
-    # Discretize the operator.
+    ω = ω_weights(z_ex, θ, 1)
+
+    # Everything to follow will use z instead of z_ex 
+    # Differential objects
     bc = (Mixed(1), Mixed(1)) # boundary conditions for differential operators
     L_1_minus = L₁₋(z, bc) # use backward difference as the drift is negative
     L_2 = L₂(z, bc) 
+
     # Calculate the stationary solution.
     r_T = r(T)
     g_T = g(T)
     A_T = (r_T - μ - υ^2/2)*I - (μ + υ^2 - g_T)*L_1_minus - υ^2/2 * L_2 # (17)
     v_T = A_T \ π.(T, z) # (24)
+    
     # Bundle as before.
-    p = (L_1 = L_1_minus, L_2 = L_2, z = z, g = g, r = r, υ = υ, π = π, T = T, μ = μ, g_T = g_T, P = P, x = x, ω = ω)
+    Ξ₁ = 1/(1 - ξ*(z[1] - 0.0)) # (A.11)
+    p = (Ξ₁ = Ξ₁, L_1 = L_1_minus, L_2 = L_2, z = z, g = g, r = r, υ = υ, π = π, T = T, μ = μ, g_T = g_T, P = P, x = x, ω = ω)
+    
     # Other objects
     u_T = [v_T; g_T]
     resid_M1 = zeros(P+1)
@@ -47,7 +57,8 @@ function calculate_residuals(ode_prob, x, ω, ode_solve_algorithm, ts) # To keep
     g_ts = zeros(length(ts))
     for (i, t) in enumerate(ts)
         v_t = sol(t)[1:P] # i.e., the value function at the point.
-        residuals[i] = v_t[1] + x(t) - dot(ω, v_t) # value matching condition (23)
+        # TODO: FIGURE OUT BEST WAY TO PASS in Ξ₁ TO THIS FUNCTION 
+        residuals[i] = Ξ₁*v_t[1] + x(t) - dot(ω, v_t) # value matching condition (27)
         v_ts[:,i] = v_t
         g_ts[i] = sol(t)[end]
     end
