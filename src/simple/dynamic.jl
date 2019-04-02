edbb05f11a3b428d1b98d380a4d257bf0dc2a71e
Arnav Sood <soodarnav01@gmail.com>
2019-04-02 15:34:05 -0700

    equation #s

--- src/simple/dynamic.jl
+++ src/simple/dynamic.jl
@@ -5,12 +5,12 @@ function f!_simple(resid,du,u,p,t)
     # Carry out calculations.
     v_t = u[1:P]
     g_t = u[P+1]
-    A = (r(t) - μ - υ^2/2)*I - (μ + υ^2 - g_t)*L_1 - υ^2/2 * L_2 # (17)
+    A = (r(t) - μ - υ^2/2)*I - (μ + υ^2 - g_t)*L_1 - υ^2/2 * L_2 # (19)
 
     # Calculate residuals.
-    resid[1:P] .= A * v_t - π.(t, z) # (26)
-    resid[1:P] .-= du[1:P] # discretized system of ODE for v (26)
-    resid[P+1] = Ξ₁*v_t[1] - dot(ω, v_t) + x(t) # value matching condition (27)
+    resid[1:P] .= A * v_t - π.(t, z) # (28)
+    resid[1:P] .-= du[1:P] # discretized system of ODE for v (28)
+    resid[P+1] = Ξ₁*v_t[1] - dot(ω, v_t) + x(t) # value matching condition (29)
 end
 
 # DAE constructor
@@ -33,11 +33,11 @@ function simpleDAE(params, settings)
     # Calculate the stationary solution.
     r_T = r(T)
     g_T = g(T)
-    A_T = (r_T - μ - υ^2/2)*I - (μ + υ^2 - g_T)*L_1_minus - υ^2/2 * L_2 # (17)
-    v_T = A_T \ π.(T, z) # (24)
+    A_T = (r_T - μ - υ^2/2)*I - (μ + υ^2 - g_T)*L_1_minus - υ^2/2 * L_2 # (19)
+    v_T = A_T \ π.(T, z) # (30)
 
     # Bundle as before.
-    Ξ₁ = 1/(1 - 1*(z[1] - 0.0)) # (A.11)
+    Ξ₁ = 1/(1 - 1*(z[1] - 0.0)) # (24)
     p = (Ξ₁ = Ξ₁, L_1 = L_1_minus, L_2 = L_2, z = z, g = g, r = r, υ = υ, π = π, T = T, μ = μ, g_T = g_T, P = P, x = x, ω = ω)
 
     # Other objects
@@ -57,7 +57,7 @@ function solve_simple_dae(ode_prob, x, ω, ode_solve_algorithm, ts) # To keep th
     g_ts = zeros(length(ts))
     for (i, t) in enumerate(ts)
         v_t = sol(t)[1:P] # i.e., the value function at the point.
-        residuals[i] = Ξ₁*v_t[1] + x(t) - dot(ω, v_t) # value matching condition (27)
+        residuals[i] = Ξ₁*v_t[1] + x(t) - dot(ω, v_t) # value matching condition (29)
         v_ts[:,i] = v_t
         g_ts[i] = sol(t)[end]
     end
--- src/simple/stationary.jl
+++ src/simple/stationary.jl
@@ -22,32 +22,32 @@ function stationary_numerical_simple(params, z_ex)
     @unpack μ, υ, θ, r, ζ, π = params
 
     # Define the pdf of the truncated exponential distribution
-    ω = ω_weights(z_ex, θ, 1) # (24), see utils/quadrature.jl
+    ω = ω_weights(z_ex, θ, 1) # (26), see utils/quadrature.jl
 
     # Differential objects
     bc = (Mixed(1), Mixed(1)) # boundary conditions for differential operators
     L_1_minus = L₁₋(z_ex, bc) # use backward difference as the drift is negative
     L_2 = L₂(z_ex, bc)
 
-    Ξ₁ = 1/(1 - 1*(z[1] - 0.0)) # (A.11)
+    Ξ₁ = 1/(1 - 1*(z[1] - 0.0)) # (24)
 
     # Function we're solving
     function stationary_numerical_given_g(in)
         g = in[1]
         # Construct the aggregate operator.
-        A = (r - μ - υ^2/2)*I - (μ + υ^2 - g)*L_1_minus - υ^2/2 * L_2 # (17)
-        v = A \ π.(z) # discretized system of ODE for v, where v'(T) = 0 (24)
-        diff = Ξ₁*v[1] - dot(ω, v) + ζ # value matching condition (29)
+        A = (r - μ - υ^2/2)*I - (μ + υ^2 - g)*L_1_minus - υ^2/2 * L_2 # (19)
+        v = A \ π.(z) # discretized system of ODE for v, where v'(T) = 0 (30)
+        diff = Ξ₁*v[1] - dot(ω, v) + ζ # value matching condition (31)
         return diff
     end
 
     # Find and validate the root
     sol = solve_system(stationary_numerical_given_g, [0.1])
     g_T = sol[1]
-    @assert(μ + υ^2/2 - g_T < 0) # Negative drift condition (18)
+    @assert(μ + υ^2/2 - g_T < 0) # Negative drift condition (20)
 
     # Use the g_T to recreate L_T and v_T
-    A_T = (r - μ - υ^2/2)*I - (μ + υ^2 - g_T)*L_1_minus - υ^2/2 * L_2 # (17)
-    v_T = A_T \ π.(z) # (24)
+    A_T = (r - μ - υ^2/2)*I - (μ + υ^2 - g_T)*L_1_minus - υ^2/2 * L_2 # (19)
+    v_T = A_T \ π.(z) # (30)
     return (g = g_T, v = v_T)
 end
