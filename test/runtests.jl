89ac49bf71beb9527ca76aa7d16eea14bd3dfff8
Arnav Sood <soodarnav01@gmail.com>
2019-04-02 13:27:18 -0700

    simple dynamic

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Sundials, BenchmarkTools, Interpolations, Parameters, SimpleDifferentialOp
 @elapsed begin
   @time @testset "Simple Stationary" begin include("simple/stationarytest.jl") end
   # @time @testset "Full Stationary" begin include("full/stationarytest.jl") end
-  # @time @testset "Simple Dynamic" begin include("simple/residualstest.jl") end
+  @time @testset "Simple Dynamic" begin include("simple/dynamictest.jl") end
   # @time @testset "Full Dynamic" begin include("full/dynamictest.jl") end
   # @time @testset "Entry residuals" begin include("full/entryresidualstest.jl") end
   # @time @testset "Type Stability and Speed" begin include("benchmarking/inference.jl") end
