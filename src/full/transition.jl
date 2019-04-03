e8a078b4c8c6a361670b4e7428c5dd41ba1dafa2
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2019-04-03 16:45:56 -0700

    Split up NLopt objective in `solve_full_model`
    Resolves #264

--- src/full/transition.jl
+++ src/full/transition.jl
@@ -56,8 +56,10 @@ function solve_full_model(settings; impose_E_monotonicity_constraints = true, wr
         h .= A*x
     end
     # run the local (NLopt) solver
-    E_nodes = solve_system(x -> weighted_residuals_given_E_nodes_interior(x, settings; front_nodes_appended = front_nodes_appended), settings.transition_x0;
-                    lb = nothing, ub = fill(0.0, length(settings.transition_x0)),
+    nlopt_objective(x) =  weighted_residuals_given_E_nodes_interior(x, settings; front_nodes_appended = front_nodes_appended)
+    E_nodes = solve_system(nlopt_objective;
+                    lb = nothing, 
+                    ub = fill(0.0, length(settings.transition_x0)),
                     constraints_fg! = impose_E_monotonicity_constraints ? constraints_increasing_E! : nothing,
                     algorithm = impose_E_monotonicity_constraints ? :LD_SLSQP : :LD_LBFGS,
                     iterations = settings.transition_iterations)
