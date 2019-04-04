9ca98f4a022bcce9789b786029a3649228836b05
Arnav Sood <soodarnav01@gmail.com>
2019-04-04 15:52:49 -0700

    coarsen grid for full/dynamictest.jl

--- test/full/dynamictest.jl
+++ test/full/dynamictest.jl
@@ -2,7 +2,7 @@
   # Time and space grid.
     z_min = 0.0
     z_max = 5.0
-    P = 50
+    P = 75
     T = 20.0
   # Experiment settings.
     d_0 = 5.0
@@ -40,46 +40,47 @@
     sol = solve_dynamics(params_T, stationary_T, settings, T, Ω, E)
   # Spot-checks.
     @test sol.sol.t[5] ≈ 19.840000000000003
-    @test_broken sol.results[:λ_ii][end] ≈ 0.7813233366790822
-    @test_broken sol.sol.u[4][3] ≈ 1.1499093016834008
-    @test_broken sol.sol.prob.u0[1] ≈ 1.1868000000001158
+    @test sol.results[:λ_ii][end] ≈ 0.8205696009690461
+    @test sol.sol.u[4][3] ≈ 0.713179042868647
+    @test sol.sol.prob.u0[1] ≈ 0.9329809580656181
   # Consistency checks.
-    @test_broken mean(sol.results[:g]) ≈ 0.020419880565858414 # Probably the most important of these checks.
-    @test_broken mean(sol.results[:z_hat]) ≈ 1.425712535030436
-    @test_broken mean(sol.results[:Ω]) ≈ 1.036549465116929
+    @test mean(sol.results[:g]) ≈ 0.006332516508877774 # Probably the most important of these checks.
+    @test mean(sol.results[:z_hat]) ≈ 1.626173967797714
+    @test mean(sol.results[:Ω]) ≈ 1.6928323073432825
     @test all(sol.results[:E] .== 0.053)
-    @test_broken mean(sol.results[:S]) ≈ 0.05847503367633505
-    @test_broken mean(sol.results[:L_tilde]) ≈ 0.200430770186847
+    @test mean(sol.results[:S]) ≈ -0.013749473107398985
+    @test mean(sol.results[:L_tilde]) ≈ 0.1892988756097014
   # Full coverage of each column.
     @test sol.results[:t][4] == 1.5
-    @test_broken sol.results[:g][5] ≈ 0.020419880572700753
-    @test_broken sol.results[:z_hat][6] ≈ 1.4257125350016342
-    @test_broken sol.results[:Ω][7] ≈ 1.036549465116929
+    @test sol.results[:g][5] ≈ 0.006332516539866723
+    @test sol.results[:z_hat][6] ≈ 1.6261739679041103
+    @test sol.results[:Ω][7] ≈ 1.6928323073432825
     @test sol.results[:E][8] == 0.053
-    @test_broken sol.results[:v_1][9] ≈ 1.1867999999850465
-    @test_broken sol.results[:L_tilde][10] ≈ 0.20043077010213567
-    @test_broken sol.results[:λ_ii][11] ≈ 0.7813233366545765
-    @test_broken sol.results[:c][12] ≈ 1.1882939932974685
-    @test_broken sol.results[:S][4] ≈ 0.05847503375286956
-    @test_broken sol.results[:z_bar][3] ≈ 1.4861677378016562
-    @test_broken sol.results[:π_min][2] ≈ 0.16549999774577948
-    @test sol.results[:entry_residual][12] ≈ -1.3211653993039363e-13 atol = 1e-5
+    @test sol.results[:v_1][9] ≈ 0.9329809601676073
+    @test sol.results[:L_tilde][10] ≈ 0.18929887587415023
+    @test sol.results[:λ_ii][11] ≈ 0.8205696004618322
+    @test sol.results[:c][12] ≈ 0.9548509646788975
+    @test sol.results[:S][4] ≈ -0.013749472948335131
+    @test sol.results[:z_bar][3] ≈ 1.1778088573728906
+    @test sol.results[:π_min][2] ≈ 0.14649422762537112
+    @test sol.results[:entry_residual][12] ≈ 2.7166018323754315e-9 atol = 1e-5
 
-  # Run the solver for another case.
-    sol = solve_dynamics(params_0, stationary_0, settings, T, Ω, E)
-  # Spot-checks.
-    @test sol.sol.t[5] == 19.852
-    @test_broken sol.results[:λ_ii][1] ≈ 0.9929472025880611
-    @test_broken sol.sol.u[4][3] ≈ 1.153063927522336
-    @test_broken sol.sol.prob.u0[1] ≈ 1.1868000000002454
-  # Detailed checks.
-    @test_broken sol.results[:g][1] ≈ 0.020019475192487802 # g check.
-    @test_broken sol.results[:z_hat][1] ≈ 2.771561823423923
-    @test_broken sol.results[:z_hat][(end-9)] ≈ 2.770363670724641
-    # Sub-pieces of L_tilde
-    @test sol.results[:L_tilde_a] + sol.results[:L_tilde_x] + sol.results[:L_tilde_E] ≈ sol.results[:L_tilde]
-    # Check if π_rat definitions in dynamics solution and SS coincide
-    @test_broken sol.results[:π_rat][end] ≈ stationary_T.π_rat atol = 1e-3
+  # TODO: Move this to the robustness section, and keep the original 1000-node grid (it doesn't work otherwise.)
+  # # Run the solver for another case.
+  #   sol = solve_dynamics(params_0, stationary_0, settings, T, Ω, E)
+  # # Spot-checks.
+  #   @test sol.sol.t[5] == 19.852
+  #   @test_broken sol.results[:λ_ii][1] ≈ 0.9929472025880611
+  #   @test_broken sol.sol.u[4][3] ≈ 1.153063927522336
+  #   @test_broken sol.sol.prob.u0[1] ≈ 1.1868000000002454
+  # # Detailed checks.
+  #   @test_broken sol.results[:g][1] ≈ 0.020019475192487802 # g check.
+  #   @test_broken sol.results[:z_hat][1] ≈ 2.771561823423923
+  #   @test_broken sol.results[:z_hat][(end-9)] ≈ 2.770363670724641
+  #   # Sub-pieces of L_tilde
+  #   @test sol.results[:L_tilde_a] + sol.results[:L_tilde_x] + sol.results[:L_tilde_E] ≈ sol.results[:L_tilde]
+  #   # Check if π_rat definitions in dynamics solution and SS coincide
+  #   @test_broken sol.results[:π_rat][end] ≈ stationary_T.π_rat atol = 1e-3
 end
 
 @testset "Regression Tests with time-varying Ω" begin
@@ -90,33 +91,34 @@ end
     sol = solve_dynamics(params_T, stationary_T, settings, T, Ω_t, E_t)
 
     # Spot-checks.
-    @test_broken sol.sol.t[5] ≈ 4.0820840627115444 atol = 1e-5
-    @test_broken sol.results[:λ_ii][end] ≈ 0.7813233366790822 atol = 1e-5
-    @test_broken sol.sol.u[4][3] ≈ 1.1499297704103377 atol = 1e-5
-    @test_broken sol.sol.prob.u0[1] ≈ 1.1868000000001158 atol = 1e-5
+    @test sol.sol.t[5] ≈ 3.532316848641289 atol = 1e-5
+    @test sol.results[:λ_ii][end] ≈ 0.8205696009690461 atol = 1e-5
+    @test sol.sol.u[4][3] ≈ 0.713186458707734 atol = 1e-5
+    @test sol.sol.prob.u0[1] ≈ 0.9329809580656181atol = 1e-5
     # Consistency checks.
-    @test_broken mean(sol.results[:g]) ≈ 0.032776838190899334 atol = 1e-5 # Probably the most important of these checks.
-    @test_broken mean(sol.results[:z_hat]) ≈ 1.4240459625635837 atol = 1e-5
-    @test_broken mean(sol.results[:Ω]) ≈ 1.1546007592192749 atol = 1e-5
-    @test_broken mean(sol.results[:S]) ≈ 0.12182791761996183 atol = 1e-5
-    @test_broken mean(sol.results[:L_tilde]) ≈ 0.1068149526037354 atol = 1e-5
+    @test mean(sol.results[:g]) ≈ 0.020144154416595162 atol = 1e-5 # Probably the most important of these checks.
+    @test mean(sol.results[:z_hat]) ≈ 1.6118214712011016 atol = 1e-5
+    @test mean(sol.results[:Ω]) ≈ 1.8255265725137522 atol = 1e-5
+    @test mean(sol.results[:S]) ≈ 0.05706141328167729 atol = 1e-5
+    @test mean(sol.results[:L_tilde]) ≈ 0.0926087309072632 atol = 1e-5
     # Full coverage of each column.
-    @test_broken sol.results[:t][4] ≈ 1.2180412122607018 atol = 1e-5
-    @test_broken sol.results[:g][5] ≈ 0.04344370969889273 atol = 1e-5
-    @test_broken sol.results[:z_hat][6] ≈ 1.4138759332401472 atol = 1e-5
-    @test_broken sol.results[:Ω][7] ≈ 1.0852902581449317 atol = 1e-5
-    @test_broken sol.results[:v_1][9] ≈ 1.2162498772031762 atol = 1e-5
-    @test_broken sol.results[:L_tilde][10] ≈ 0.18748845694858562 atol = 1e-5
-    @test_broken sol.results[:λ_ii][11] ≈ 0.7810856222518524 atol = 1e-5
-    @test_broken sol.results[:c][12] ≈ 1.1899458996247958 atol = 1e-5
-    @test_broken sol.results[:S][4] ≈ 0.18806483288417494 atol = 1e-5
-    @test_broken sol.results[:z_bar][3] ≈ 1.6350019300336807 atol = 1e-5
-    @test_broken sol.results[:π_min][2] ≈ 0.21425367016810465 atol = 1e-5
-    @test_broken sol.results[:entry_residual][12] ≈ 0.0010887433781643363 atol = 1e-5
+    @test sol.results[:t][4] ≈ 0.8318036450026267 atol = 1e-5
+    @test sol.results[:g][5] ≈ 0.03529457859081019 atol = 1e-5
+    @test sol.results[:z_hat][6] ≈ 1.5963201866789414 atol = 1e-5
+    @test sol.results[:Ω][7] ≈ 1.7902142064165565 atol = 1e-5
+    @test sol.results[:v_1][9] ≈ 0.98515301205966 atol = 1e-5
+    @test sol.results[:L_tilde][10] ≈ 0.160812673571405 atol = 1e-5
+    @test sol.results[:λ_ii][11] ≈ 0.8197747097913394 atol = 1e-5
+    @test sol.results[:c][12] ≈ 0.9667053570460247 atol = 1e-5
+    @test sol.results[:S][4] ≈ 0.14915482412907646 atol = 1e-5
+    @test sol.results[:z_bar][3] ≈ 1.2083652214290672 atol = 1e-5
+    @test sol.results[:π_min][2] ≈ 0.2042841017741823 atol = 1e-5
+    @test sol.results[:entry_residual][12] ≈ 0.013589358773159477 atol = 1e-5
     # Sub-pieces of L_tilde
     @test sol.results[:L_tilde_a] + sol.results[:L_tilde_x] + sol.results[:L_tilde_E] ≈ sol.results[:L_tilde]
+    # TODO: is this still valid with a coarse grid? 
     # Check if π_rat definitions in dynamics solution and SS coincide
-    @test_broken sol.results[:π_rat][end] ≈ stationary_T.π_rat atol = 1e-3
+    # @test_broken sol.results[:π_rat][end] ≈ stationary_T.π_rat atol = 1e-3
 end
 
 @testset "Correctness Tests" begin # Here, we compare the DAE output to known correct values, such as MATLAB output or analytical results.
@@ -124,9 +126,9 @@ end
   sol = solve_dynamics(params_T, stationary_T, settings, T, Ω, E)
   @test all([isapprox(x, 0.0, atol = 1e-6) for x in sol.results[:entry_residual]]) # Free-entry condition holds ∀ t.
 
-  # Second case.
-  sol = solve_dynamics(params_0, stationary_0, settings, T, Ω, E)
-  # @show all([isapprox(x, 0.0, atol = 1e-6) for x in sol.results[:entry_residual]]) # Free-entry condition holds ∀ t.
+  # TODO: Move this to robustness, with the original fine grid.
+  # sol = solve_dynamics(params_0, stationary_0, settings, T, Ω, E)
+  # @test all([isapprox(x, 0.0, atol = 1e-6) for x in sol.results[:entry_residual]]) # Free-entry condition holds ∀ t.
 end
 
 @testset "Regression Tests for f! at T" begin
@@ -146,7 +148,7 @@ end
       @test all(abs.(resid) .<= 1e-6) # Test that we have small residuals across the board.
     # Regression
       @test resid[4] ≈ 0 atol = 1e-6
-      @test resid[100] ≈ 0 atol = 1e-6
+      @test resid[end] ≈ 0 atol = 1e-6
     # Benchmarking results
       println("Kernel Benchmarking")
       @show @btime f!($resid, $du, $u, $p, $t)
